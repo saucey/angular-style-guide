b09281a4ef8a779eb8645dc3022a1cb8198e2412
Rocco R <xdemocle@gmail.com>
2015-02-26 15:21:10 +0100

    moving button.js to exmaple.js and adding some example for proprietary Drupal's methods: attach and detach

--- components/example.js
+++ components/example.js
@@ -7,13 +7,19 @@
   'use strict';
 
   // Add new item to public Drupal object
-  Drupal.behaviors.button_example = {
+  Drupal.behaviors.example = {
     attach: function () {
 
       var button_var = {};
 
       return button_var;
     }
+  };
+
+  // You could add additional behaviors here.
+  Drupal.behaviors.myModuleMagic = {
+    attach: function (context, settings) { },
+    detach: function (context, settings) { }
   };
 
 })(jQuery);
