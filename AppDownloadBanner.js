7e910091b769770abbec89979265b96e688e57de
Rocco R <xdemocle@gmail.com>
2015-03-20 22:09:37 +0100

    adding Smart App Download banner plus refactoring for this old implementation with support of android and ios

---
+++ AppDownloadBanner.js
@@ -0,0 +1,23 @@
+(function ($) {
+
+  'use strict';
+
+  $.smartbanner({
+    // days to hide banner after close button is clicked (defaults to 15)
+    daysHidden: 15,
+    // days to hide banner after "VIEW" button is clicked (defaults to 90)
+    daysReminder: 90,
+    // language code for the App Store (defaults to us)
+    appStoreLanguage: 'nl',
+    title: 'Mijn Aegon',
+    author: 'AEGON Nederland N.V.',
+    button: 'Bekijk',
+    store: {
+      ios: 'On the App Store',
+      android: 'In Google Play'
+    },
+    price: 'Gratis'
+    // , force: 'android' // Uncomment for platform emulation
+  });
+
+})(jQuery);
