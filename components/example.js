3c2269853599370225f19a3d62293f551d90d1b5
Rocco R <xdemocle@gmail.com>
2015-03-20 22:08:49 +0100

    commenting example code

--- components/example.js
+++ components/example.js
@@ -3,23 +3,23 @@
  */
 
 // Closure with jQuery support
-(function($) {
-  'use strict';
-
-  // Add new item to public Drupal object
-  Drupal.behaviors.example = {
-    attach: function () {
-
-      var button_var = {};
-
-      return button_var;
-    }
-  };
-
-  // You could add additional behaviors here.
-  Drupal.behaviors.myModuleMagic = {
-    attach: function (context, settings) { },
-    detach: function (context, settings) { }
-  };
-
-})(jQuery);
+// (function($) {
+//   'use strict';
+// 
+//   // Add new item to public Drupal object
+//   Drupal.behaviors.example = {
+//     attach: function () {
+// 
+//       var button_var = {};
+// 
+//       return button_var;
+//     }
+//   };
+// 
+//   // You could add additional behaviors here.
+//   Drupal.behaviors.myModuleMagic = {
+//     attach: function (context, settings) { },
+//     detach: function (context, settings) { }
+//   };
+// 
+// })(jQuery);
