d5ccc417288381f8c3670c5df9cd69fc78089e6b
Rocco R <xdemocle@gmail.com>
2015-03-27 21:50:46 +0100

    commit behalf Jorg with refactor for merging the changes

--- widgets/lhfs.js
+++ widgets/lhfs.js
@@ -13,11 +13,33 @@
       // Check if div#lhfs_widget exist
       if ($('#lhfs_widget').length > 0) {
 
-        // Run your DOM manipulation stuff
-        $(".tip").removeClass("visible");
-        $(".tip").addClass("visible");
-        $("li.payment").removeClass("data");
-        $("li.payment").addClass("data");
+        window.edit = function (show) {
+          $('.details').toggleClass('edit', show);
+          $('.actions').toggleClass('edit', show);
+        }
+
+        //$("#lhfs_widget .tip").removeClass("visible");
+        //$("#lhfs_widget .tip").addClass("visible");
+        //$("#lhfs_widget li.payment").removeClass("data");
+        //$("#lhfs_widget li.payment").addClass("data");
+        $("#lhfs_widget li.payment ul.horizontal").removeClass("visible");
+        $("#lhfs_widget li.payment ul.horizontal").addClass("visible");
+        //$($("#lhfs_widget li.payment ul.horizontal")[0]).addClass("visible");
+        $("#lhfs_widget li.payment ul.horizontal.error").removeClass("visible");
+
+        $(".lhfs_widget select").msDropDown();
+
+        //transfer classes from select to root object of msDropDown (conserve .half, for example)
+        $(".ddOutOfVision > select").each(function() {
+          $(this).parent().next().addClass(this.className);
+        });
+
+        //put an event listener on the invalid element for inputs (to display the error text, if necessary)
+        $("input").bind("invalid", function(event) {
+          console.log($(this).closest("ul").prev("ul.error").addClass("visible"));
+        });
+
+        $("form [name=lhfs_form]").validate({errorClass: "errorText"});
       }
     }
   };
