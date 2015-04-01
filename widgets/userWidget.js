2cbd9620cbb93fa7626846e675c776fcb2fdcb95
Rocco R <xdemocle@gmail.com>
2015-04-01 18:47:13 +0200

    user widget

---
+++ widgets/userWidget.js
@@ -0,0 +1,16 @@
+/**
+ * User details script
+ */
+
+(function($) {
+  'use strict';
+
+  // Add new item to public Drupal object
+  Drupal.behaviors.userWidget = {
+
+    attach: function () {
+
+    }
+  };
+
+})(jQuery);
