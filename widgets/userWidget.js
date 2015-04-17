99317db12705d4e39a0be8bbb43fe595612a2283
Rocco R <xdemocle@gmail.com>
2015-04-17 19:19:34 +0200

    enable simulation for all websites

--- widgets/userWidget.js
+++ widgets/userWidget.js
@@ -13,8 +13,8 @@
 
       // TEMP: stop everything in case we aren't under vps-rhino-1 or localhost
       // if (doc.domain === '10.120.32.22') { return; }
-      if (doc.domain.search('rhino.aegon.nl') === -1) { return; }
-      if (doc.domain.search('localhost') === -1) { return; }
+      // if (doc.domain.search('rhino.aegon.nl') === -1) { return; }
+      // if (doc.domain.search('localhost') === -1) { return; }
 
       // Setup
       this.setup(settings);
@@ -32,8 +32,21 @@
 
     showLogged: function () {
 
-      $("#shw-user-details").load(this.apiUrl + 'user');
-      $("#shw-contract-overview").load(this.apiUrl + 'contract_overview');
+      // $("#shw-user-details").load(this.apiUrl + 'user');
+      // $("#shw-contract-overview").load(this.apiUrl + 'contract_overview');
+      $('#shw-user-details').html('<div id="user_widget" class="user_widget">
+      <div id="user_widget_topmenu" class="topmenu">
+        <div class="username">Michiel van der Most Spijk</div>
+      </div>
+
+      <div id="user_widget_dropdown" class="frame bubbleTopRight">
+        <div class="username">user name</div>
+        <div class="email">usermail@somesite.com</div>
+        <div class="lastlogin">Uw vorige bezoek was op 08-01-2015 om 14:11 uur</div>
+        <button class="arrow">Mijn overzicht</button>
+        <button class="white">uitloggen</button>
+      </div>
+    </div>');
     },
 
     simulation: function () {
