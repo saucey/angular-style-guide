13def389c88b941d814393541b852258805a6732
Rocco Russo <xdemocle@gmail.com>
2015-04-23 23:58:37 +0200

    removing user logged simulation and setup api

--- widgets/userWidget.js
+++ widgets/userWidget.js
@@ -17,7 +17,7 @@
       // if (doc.domain.search('localhost') === -1) { return; }
 
       // Setup
-      this.setup(settings);
+      // this.setup(settings);
       
       // Simulation TEMP
       this.simulation();
@@ -34,41 +34,43 @@
 
       // $("#shw-user-details").load(this.apiUrl + 'user');
       // $("#shw-contract-overview").load(this.apiUrl + 'contract_overview');
-      $('#shw-user-details').html('<div id="user_widget" class="user_widget">
-      <div id="user_widget_topmenu" class="topmenu">
-        <div class="username">Michiel van der Most Spijk</div>
-      </div>
+      $('#shw-user-details').html('
+        <div id="user_widget" class="user_widget">
+          <div id="user_widget_topmenu" class="topmenu">
+            <div class="username">Michiel van der Most Spijk</div>
+          </div>
 
-      <div id="user_widget_dropdown" class="frame bubbleTopRight">
-        <div class="username">user name</div>
-        <div class="email">usermail@somesite.com</div>
-        <div class="lastlogin">Uw vorige bezoek was op 08-01-2015 om 14:11 uur</div>
-        <button class="arrow">Mijn overzicht</button>
-        <button class="white">uitloggen</button>
-      </div>
-    </div>');
+          <div id="user_widget_dropdown" class="frame bubbleTopRight">
+            <div class="username">user name</div>
+            <div class="email">usermail@somesite.com</div>
+            <div class="lastlogin">Uw vorige bezoek was op 08-01-2015 om 14:11 uur</div>
+            <button class="arrow">Mijn overzicht</button>
+            <button class="white">uitloggen</button>
+          </div>
+        </div>
+      ');
     },
 
     simulation: function () {
 
       var that = this;
 
-      // that.showLogged();
+      that.showLogged();
 
-      if (location.pathname.indexOf('mijnaegon') === -1) {
-        return;
-      }
+      // if (location.pathname.indexOf('mijnaegon') === -1) {
+      //   return;
+      // }
 
-      setTimeout(function () {
+      // setTimeout(function () {
 
-        if (window.confirm('Do you want simulate session logged?')) { 
+      //   if (window.confirm('Do you want simulate session logged?')) { 
 
-          setTimeout(function () {
-            that.showLogged();
-          }, 1000);
-        }
+      //     setTimeout(function () {
+      //       that.showLogged();
+      //     }, 1000);
+      //   }
 
-      }, 2000);
+      // }, 2000);
     }
   };
 
