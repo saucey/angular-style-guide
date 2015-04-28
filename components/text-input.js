e7858b031c93739cdf977f2e24c8cfdaea95ed2d
yogibimbi <yogibimbi@qan.be>
2015-04-28 11:19:46 +0200

    deactivate debug message

--- components/text-input.js
+++ components/text-input.js
@@ -8,8 +8,8 @@
   // Add new item to public Drupal object
   Drupal.behaviors["text-input"] = {
     attach: function () {
-    	var visited = function () {
-    		console.log("visiting");
+    	var visited = function () {  //this is to implement a different error behaviour between when the form is loaded and once an element has been visited
+//console.log("visiting");
     		if ($(this).is(".dd")) {
     			$(this).find("select").addClass("visited");
     		}
@@ -17,8 +17,8 @@
         		$(this).addClass("visited");
         	}
         };
-        $("input, textarea").focus(visited);
-        $("input, select, textarea, div.dd, span.checkbox").click(visited);
+      $("input, textarea").focus(visited);
+      $("input, select, textarea, div.dd, span.checkbox").click(visited); //strictly speaking, this line with only the checkbox selector should be put into checkbox.js as well, but accompanied by all the previous lines this seems to be an awful lot of baggage just to maintain the appearance of modularity
     }
   };
 })(jQuery);
