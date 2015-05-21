1c17335f337fa54e76bf1828e939eab1665f5772
Rocco Russo <im@rocco.me>
2015-05-22 01:50:26 +0200

    add initial my_documents files

---
+++ widgets/myDocuments.js
@@ -0,0 +1,33 @@
+/*jshint multistr: true */
+/**
+ * User details script
+ * Dependencies: 
+ */
+
+(function(doc, win, $, Drupal) {
+
+  'use strict';
+
+  /**
+   * Register or retrieve the public container for our shw widgets.
+   * Purpose of this window global object is for register some public functions.
+   * Need to be initialized on top of each widgets.
+   */
+  win.shwGlobal = win.shwGlobal || {};
+
+  /**
+   * MyDocuments's configuration
+   */
+
+  /**
+   * MyDocuments's Drupal script.
+   * Add new item to public Drupal object
+   */
+  Drupal.behaviors.myDocuments = {
+
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(this.document, this, this.jQuery, this.Drupal);
