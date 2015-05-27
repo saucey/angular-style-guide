84369f70a5fefe3fd056149431a73a690c67cc6d
Rocco Russo <im@rocco.me>
2015-05-27 14:13:44 +0200

    minor refactor

--- widgets/myDocuments.js
+++ widgets/myDocuments.js
@@ -1,31 +1,19 @@
 /*jshint multistr: true */
 /**
- * User details script
- * Dependencies: 
+ * Mijn Documenten widget script
+ * Dependencies: null
  */
-
 (function(doc, win, $, Drupal) {
 
   'use strict';
 
   /**
-   * Register or retrieve the public container for our shw widgets.
-   * Purpose of this window global object is for register some public functions.
-   * Need to be initialized on top of each widgets.
-   */
-  win.shwGlobal = win.shwGlobal || {};
-
-  /**
-   * MyDocuments's configuration
-   */
-
-  /**
    * MyDocuments's Drupal script.
    * Add new item to public Drupal object
    */
-  Drupal.behaviors.myDocuments = {
+  Drupal.behaviors.myDocumentsWidget = {
 
-    attach: function (context, settings) {
+    attach: function () {
 
       $('.my_documents_widget article h2').on('click', function () {
         $(this).parent('article').toggleClass('open')
