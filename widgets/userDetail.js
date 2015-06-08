9308dc1b28de2cd5edda720ea29f896188bd9745
yogibimbi <yogibimbi@qan.be>
2015-06-08 14:33:25 +0200

    deleting the hasBeenShown-cookie upon logout, Jira #CXDEVII-863

--- widgets/userDetail.js
+++ widgets/userDetail.js
@@ -216,7 +216,7 @@
 
         // Convert lastAcess in formatted date
         dateFormatted = this.formatDatetime(data.lastAccess);
-console.log("date ");
+        console.log("date ");
         // Parse date time
         $template.find('span.user_detail_widget_last_access')
           .text(dateFormatted);
@@ -465,6 +465,10 @@ console.log("date ");
 
       // Remove mijn_last_login's cookie
       this.clearCookie();
+      
+      // remove the cookie that determines if the green bar is shown
+      console.log("logout");
+      $.removeCookie("hasBeenShown");
 
       // Switch off all events
       this.events(true);
