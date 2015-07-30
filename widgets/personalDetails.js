98b4c64b4ef174f405826cca4e6aac738b1f0688
yogibimbi <yogibimbi@qan.be>
2015-07-30 14:55:21 +0200

    cleaned up personalDetails code, added .attached property, Jira#CXDEVII-1266

--- widgets/personalDetails.js
+++ widgets/personalDetails.js
@@ -7,21 +7,11 @@
 
   'use strict';
 
-  var testSelector = function (selector) {
-    document.querySelector('*');  //checks if querySelector is implemented and raises an error if not
-    try {document.querySelector(selector)} catch (e) {return false}
-    return true;
-  }
-
   /**
    * MyPersonalDetails's Drupal script.
    * Add new item to public Drupal object
    */
   Drupal.behaviors.personalDetailsWidget = {
-    validation: {  //  this is part of the form validation routine; when it is working correctly, this should be bumped to a general level
-      zip: "^d+\w*$",
-    },
-
     attach: function () {
       Drupal.behaviors.tooltip.activate("#personal_details_widget");
 
@@ -42,7 +32,9 @@
         $(".address .correspondential .world").toggleClass("visible", !NL);
       });
       $("input[name=ca_NL]:checked").click();
-    }
+      this.attached = true;
+    },
+    attached: false,
   };
 
 })(this.document, this, this.jQuery, this.Drupal);
