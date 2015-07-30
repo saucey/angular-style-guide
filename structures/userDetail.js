6e86e944665cbd88598901146f99639e8736849e
Tjerk Heringa <tjerk@dotnetdesign.nl>
2015-07-30 09:09:00 +0200

    CXDEVII-1056: reverting

--- structures/userDetail.js
+++ structures/userDetail.js
@@ -234,10 +234,6 @@
 
     parseWidget: function (data, callback) {
 
-      // reload page
-      // Added argument true to bypass cache in MSIE9
-      location.reload(true);
-
       // Vars for local scope
       var $template, dateFormatted;
 
