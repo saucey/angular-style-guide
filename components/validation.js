fab81706002c6ea4f2cd75896ea37bf691da7ec6
yogibimbi <yogibimbi@qan.be>
2015-08-03 20:17:45 +0200

    first implementation of a more general form validation routine, using the attribute data-validate and the array Drupal.behaviors.validation.validators

--- components/validation.js
+++ components/validation.js
@@ -8,9 +8,38 @@
   // Add new item to public Drupal object
   Drupal.behaviors.validation = {
     attach: function () {
+      this.init();
       this.attached = true;  //used to determine if this function has already run
     },
     attached: false,
+
+    init: function () {
+      var that = this;
+      $("[data-validate]").each( function () {
+        var v = this.attributes['data-validate'].value;
+        // check if validator key is in the form [key], if not, it needs a . in front for eval
+        v = v[0] === "[" ? v : "." + v;
+        try {
+          var validator = eval("that.validators" + v) || (v.bla.bla); // if validator is empty, also throw an error
+        }
+        catch (e) {
+          window.console && window.console.warn("error getting validator " + v + ". Maybe it was not defined?");
+        }
+        
+        // react to different formats of validator
+        switch (typeof validator) {
+          case "function":
+
+            break;
+          case "object": //regex
+            // get rid of the / / around the pattern
+            validator = validator.source;
+            // if there is already a pattern defined, this takes prevalence
+            this.pattern = this.pattern || validator;
+            break;
+        }
+      });
+    },
 
     testSelector: function (selector) {
       if (!document.querySelector) {    //checks if querySelector is implemented and raises an error if not
@@ -47,6 +76,12 @@
       catch (e) {
         return $(form_selector).find("input.invalid.blurred, select.invalid.blurred, textarea.invalid.blurred").length;        
       }
+    },
+    // nested validators are possible, enclose non - \w - names in ['']
+    validators: {
+      ['zip']: {
+        nl: /^\s*\d{4}\s*[a-zA-Z]{2}\s*$/,
+      },
     }
   };
 })(jQuery);
