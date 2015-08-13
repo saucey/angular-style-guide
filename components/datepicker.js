6de7f9076ea1555daa1defcd6eaa90d65df88408
Tjerk Heringa <tjerk@dotnetdesign.nl>
2015-08-13 15:07:50 +0200

    Started on date picker

--- components/datepicker.js
+++ components/datepicker.js
@@ -1,2 +1,23 @@
+/**
+ * Example JavaScript component
+ */
+// Closure with jQuery support
+(function($) {
+  'use strict';
+
+  // Add new item to public Drupal object
+  Drupal.behaviors.datepicker = {
+    attach: function () {
+      $( "#datepicker" ).datepicker({
+      showButtonPanel: true,
+      minDate: -20, 
+      maxDate: "+1M +10D"
+    });
+
+      this.attached = true;  //used to determine if this function has already run
+    },
+    attached: false,
+  };
+})(jQuery);
 
 
