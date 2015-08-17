af9606b0a141bba68d711f7212a914290aea7b27
Tjerk Heringa <tjerk@dotnetdesign.nl>
2015-08-17 10:49:14 +0200

    Working on date picker

--- components/datepicker.js
+++ components/datepicker.js
@@ -2,24 +2,27 @@
  * Example JavaScript component
  */
 // Closure with jQuery support
+
+// datepicker
+
 (function($) {
   'use strict';
 
   // Add new item to public Drupal object
-  Drupal.behaviors.datepicker = {
-    attach: function () {
-      $( "#datepicker" ).datepicker({
+    Drupal.behaviors.slider = {
+      attach: function () {
+      $(function() {
+        $( "#datepicker" ).datepicker({
       showButtonPanel: true,
       minDate: -20, 
       maxDate: "+1M +10D",
       changeMonth: true,
       changeYear: true
-    });
-
-      this.attached = true;  //used to determine if this function has already run
-    },
+          });
+        });
+    }, // end attach
     attached: false,
-  };
-})(jQuery);
+  }; // end Drupal
+})(jQuery); 
 
 
