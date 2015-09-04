df39093a86fa062099272aeb6bcdc51b5a488e08
Geo Molenaar <geordy.molenaar@gmail.com>
2015-09-04 14:12:27 +0200

    CXDEVII-1544: Refactored pensioen123.js, final functionality is done.

--- widgets/pensioen123.js
+++ widgets/pensioen123.js
@@ -10,78 +10,69 @@
   Drupal.behaviors.pensioen123_widget = {
     attach: function () {
 
+      // Public function to set divs with the same class to the same height
       $.fn.setAllToMaxHeight = function(){
         return this.height( Math.max.apply(this, $.map( this , function(e){ return $(e).height() }) ) ); // jshint ignore:line
       };
 
+      // Set all divs with class span4 inside .user-details same height
       $('.user-details .span4').setAllToMaxHeight();
 
-
-      //TODO:Demo functionality, may change depending on the content we will get later
       $('.icon').click(function() {
         var that = $(this),
             icon = that.data('icon'),
             el = '.details',
-            details = that.closest('.row-fluid').find(el + "[data-icon='" + icon + "']");
+            details = that.closest('.row-fluid').find(el + "[data-icon='" + icon + "']"),
+            selectedImg = '<img src="https://s3-eu-west-1.amazonaws.com/anl-ma-staticcontent/content/pensioen123/general/selected.png" class="selected">';
 
 
-        $('.details').each(function() {
-          if($(this).hasClass('open') && $(this).data('icon') !== icon) {
-            $(this).removeClass('open');
-            $(this).slideUp('fast');
-          }
-        }).promise().done( function(){ 
-          if(!details.hasClass('open')) {
-            details.find('.more-details').removeAttr('style');
-            details.find('.more').removeAttr('style');
-          }
-          details.slideDown('fast');
-          details.addClass('open'); 
+        // Loop through all elements with the class icon
+        $('.icon').each(function() {
+          // If there is an img with the class selected, remove it from the dom
+          $(this).find('.selected').remove();
+        }).promise().done(function() {
+          // Append overlay image to the clicked icon
+          that.append(selectedImg);
         });
-        
+
+        // Open details
+        $('.details').not(details).slideUp('fast').promise().done(function() {
+          // Remove styles from .more and .more-details if they were clicked before
+          details.find('.more').removeAttr('style');
+          details.find('.more-details').removeAttr('style');
+          // Open details
+          details.slideDown('fast');
+        });
+                 
       });
 
+      // Open tab on mobile devices
+      $('.title').click(function() {
+        var that = $(this),
+            el = that.closest('.row-fluid').find('.tab');
+        
+        // Animate arrow when opening the tab
+        $('.title').not(that).removeClass('arrowup');
+        that.addClass('arrowup');
+
+        // Open selected tab
+        $('.tab').not(el).slideUp('fast').promise().done(function() {
+          el.slideDown('fast');
+        });
+      });
+
+      // Close details
       $('.close').click(function(e) {
         e.preventDefault();
         var parent = $(this).parent().parent();
         parent.slideUp('fast');
-        parent.removeClass('open');
       });
 
-      /*
-       * Clicking the more link will show the more-details div
-       */
+      // Open more-details
       $('.more').click(function(e) {
         e.preventDefault();
         $(this).closest('.details').find('.more-details').slideToggle('fast');
         $(this).hide();
-      });
-
-      /*
-       * Mobile functionality
-       * By clicking on .title the tab with icons will slide down
-       */
-      $('.title').click(function() {
-        var that = $(this),
-            el = that.closest('.row-fluid').find('.tab');
-
-        if(el.hasClass('open')) {
-          $(this).removeClass('arrowup');
-          el.removeClass('open');
-          el.slideUp('fast');
-        } else {
-          $('.tab').each(function() {
-            if($(this).hasClass('open')) {
-              $(this).closest('.row-fluid').find('.title').removeClass('arrowup');
-              $(this).removeClass('open');
-              $(this).slideUp('fast');
-            }
-          }).promise().done(function() {
-            that.addClass('arrowup');
-            el.slideDown('fast');
-            el.addClass('open');
-          });
-        }
       });
     }
   };
