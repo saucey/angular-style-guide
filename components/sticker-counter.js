41c02a0abb156ac7fe9be9bcb87855a109c727a7
Robbert van den Bogerd <rvdbogerd@bliq.net>
2015-09-23 17:42:59 +0200

    rename to sticker-counter and attach script as drupal behavior

---
+++ components/sticker-counter.js
@@ -0,0 +1,23 @@
+/**
+ * Counter: replaces all characters inside a .counter element with <span>char</span>,
+ * so all characters will be styled with a box around it.
+ */
+(function($) {
+  'use strict';
+
+  // Add new item to public Drupal object
+  Drupal.behaviors.select = {
+    attach: function (context) {
+      var $counterElement = $('.counter', context),
+          charsWithSpans = '';
+
+      $.each(($counterElement.text().trim().split('')), function (index, char) {
+        charsWithSpans += '<span>' + char + '</span>';
+      });
+
+      $counterElement.html(charsWithSpans);
+    }
+  }
+
+})(jQuery);
+
