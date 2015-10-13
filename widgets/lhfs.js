be364f5eba82a1dfa3ff22744b012f5ab2b6ee39
Geo Molenaar <geordy.molenaar@gmail.com>
2015-10-13 17:04:42 +0200

    Updated lhfs.js with logic for the dropdown

--- widgets/lhfs.js
+++ widgets/lhfs.js
@@ -11,7 +11,7 @@
     attach: function () {
 
       var Validation = Drupal.behaviors.validation;
-      var formSelector = "form[name=lhfs_form]";
+      var formSelector = "form[name=modifyBetaalGegevensForm]";
       if (!Validation.IEFix(formSelector, false)) {
         Validation.otherFix(formSelector, false);
       }
@@ -50,68 +50,118 @@
             });
           }
         });
+
+        var form = $('#lhfs_widget').find('form'),
+            $iban = form.find('ul.account'),
+            $incasso = form.find('.incasso'),
+            ibanInputField = $iban.find('input'),
+            incassoCheckbox = $incasso.find('input'),
+            selectDefaultValue = $('select').val();
+
+        var showIbanField = function() {
+            $iban.show();
+            ibanInputField.show();
+        };
+
+        var hideIbanField = function() {
+            $iban.hide();
+            ibanInputField.hide();
+        };
+
+        var showIncassoField = function() {
+            $incasso.show();
+            incassoCheckbox.show();
+        };
+
+        var hideIncassoField = function() {
+            $incasso.hide();
+            incassoCheckbox.hide();
+        };
+
+        var enableSubmitButton = function() {
+          $('.disabler').css('z-index', '2');
+        };
+
+        var disableSubmitButton = function() {
+          $('.disabler').css('z-index', '3');
+        };
+
+        ibanInputField.keyup(function() {
+          if($('select').val() === 'd' || $('select').val() === 'D') {
+            if(ibanInputField.val().length > 0 && incassoCheckbox.is(':checked')){
+              enableSubmitButton();
+            } else {
+              disableSubmitButton();
+            }
+          } else if(ibanInputField.val().length > 0){
+              enableSubmitButton();
+            } else  {
+              disableSubmitButton();
+          }
+        });
+
+        incassoCheckbox.change(function() {
+          if($('select').val() === 'd' || $('select').val() === 'D') {
+            if(ibanInputField.val().length > 0 && incassoCheckbox.is(':checked')){
+              enableSubmitButton();
+            } else {
+              disableSubmitButton();
+            }
+          } else {
+            if(incassoCheckbox.is(':checked')) {
+              enableSubmitButton();
+            } else {
+              disableSubmitButton();
+            }
+          }
+        });
+          
+        // Check for default value of the select dropdown and show/hide fields accordingly
+        if (selectDefaultValue === 'f' || selectDefaultValue === 'F') {
+            hideIbanField();
+            hideIncassoField();
+            enableSubmitButton();
+        } else if(selectDefaultValue === 'p' || selectDefaultValue === 'P' || selectDefaultValue === 'i' || selectDefaultValue === 'I'){
+            hideIncassoField();
+            // Checks if user already has a value filled in
+            if(ibanInputField.val()) {
+              enableSubmitButton();
+            } else {
+              disableSubmitButton();
+            }
+        }
+
+        // Behaviour when switching payment methods
         $("select").change(function () {
-          // var index = this.selectedIndex;
-          var $details = $(this).closest(".details");
-          var $interval = $details.find('ul.interval');
-          var $iban = $details.find("ul.account");
-          var $form = $(this).closest("form");
-          var $incasso = $form.find('.incasso');
-          var form = this.form;
           var val = $(this).val();
 
-          // $iban.toggle(index != 1);
-          // $(form).find("ul.incasso").toggle(index != 2);
-
+          // Disable submit button and uncheck checkbox by default
+          disableSubmitButton();
+          incassoCheckbox.attr('checked', false);
           // Show iban field only if value = p, d or i
-          $iban.show(val === 'p' || val === 'P' ||  val === 'd' || val === 'D' || val === 'i' || val === 'I');
+          if(val === 'p' || val === 'P' ||  val === 'd' || val === 'D' || val === 'i' || val === 'I') {
+            showIbanField();
+            hideIncassoField();
+          } 
+          // Hide iban and show checkbox
+          if(val === 'd' || val === 'D') {
+            showIbanField();
+            showIncassoField();
+          }
 
-          // Toggle incasso checkbox if value = d
-          $(form).find("ul.incasso").toggle(val === 'd' || val === 'D');       
-
-          // If value of select box = f , delete the iban field (required because of the validation)
+          // On switching between payment methods check if there is already a value filled in and enable the button
+          if(val === 'p' || val === 'P' || val === 'i' || val === 'I') {
+            if(ibanInputField.val()) {
+              enableSubmitButton();
+            } else {
+              disableSubmitButton();
+            }
+          }
+          // Hide iban and checkbox and enable submit button
           if(val === 'f' || val === 'F') {
-            $iban.remove();
-          }
-
-          // If value of select box != d , delete the incasso checkbox (required because of the validation)
-          if(val !== 'd' || val !== 'D') {
-            $incasso.remove();
-          }
-
-          // If value = d and the incasso checkbox was removed, append it again
-          if((val === 'd' || val === 'D') && $incasso.length === 0) {
-            $('.products').after('<ul class="horizontal visible edit incasso row-fluid">' +
-                                '<li class="label span6">' +
-                                '<span class="middle-enforcer"></span>&nbsp;' +
-                                '</li>' +
-                                '<li class="value sxpan6">' +
-                                '<span class="middle-enforcer"></span>' +
-                                '<span class="help"></span>' +
-                                '<label class="checkbox required">' +
-                                '<input type="checkbox" name="colCheckInput" value="lorem" required/>' +
-                                '<span class="checkbox"></span>' +
-                                '<span class="label">Payment incasso text</span>' +
-                                '<span class="errorText">some error message visible</span>' +
-                                '</label>' +
-                                '</li>' +
-                                '</ul>');
-          }
-
-          // If value != f and the iban field was removed, append it again 
-          if((val !== 'f' || val !== 'F') && $iban.length === 0) {
-            $($interval).after('<ul class="horizontal account row-fluid">' +
-                              '<li class="label required span6">' +
-                              '<span class="middle-enforcer"></span>Account number label' +
-                              '</li>' +
-                              '<li id="accountNumberField" class="value sxpan6">' +
-                              '<span class="help"></span>' +
-                              '<input type="text" name="accountNumberValue" id="accountNumberValue" data-validate="iban.nl" required/>' +
-                              '<span class="errorText">some error message visible</span>' +
-                              '</li>' +
-                              '</ul>');
-            // Set the iban field to display block after appending (by default it has display)
-            $details.find("ul.account").css('display', 'block');
+            hideIbanField();
+            hideIncassoField();
+            enableSubmitButton();
           }
           
         });
