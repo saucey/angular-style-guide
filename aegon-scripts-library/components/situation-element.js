3b25c936c2197940ad3eee856a7009a1cff93bb4
Rik Toetenel <riktoet@gmail.com>
2015-10-28 14:31:47 +0100

    Initial commit new setup. Combined updates for situation element (JS AND SASS)

---
+++ aegon-scripts-library/components/situation-element.js
@@ -0,0 +1,18 @@
+/**
+ * Copy of the anchors of the situation element to a mobile element
+ */
+(function($) {
+  'use strict';
+
+  // Add new item to public Drupal object
+  Drupal.behaviors.situation_element = {
+    attach: function () {
+      $('.situation-element').each(function() {
+        var sitationLink = $(this).find('.situation-link').clone().addClass('button arrow');
+        var mobileElement = $(this).find('.situation-text-mobile');
+        mobileElement.append(sitationLink);
+     });
+    }
+  };
+
+})(jQuery);
