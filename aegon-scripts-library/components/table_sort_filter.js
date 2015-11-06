475ca9594678c43d5e5e53edff67aaf3c25c30dc
Geo Molenaar <geordy.molenaar@gmail.com>
2015-11-06 13:17:24 +0100

    CXDEVII-1665: Removed unnecessary javascript from table_sort_filter.js

--- aegon-scripts-library/components/table_sort_filter.js
+++ aegon-scripts-library/components/table_sort_filter.js
@@ -9,75 +9,7 @@
   Drupal.behaviors.table_sort_filter = {
 
     attach: function () {
-      var wrapperId = 'sortabletable';
-      var table = $('.sortabletable').DataTable({
-        "info": true,
-        "oLanguage": {
-          "sInfo": "_END_ van _TOTAL_ documenten",
-          "sInfoEmpty": "Geen resultaten gevonden",
-          "sInfoFiltered": "",
-        },
-        "orderClasses": false,
-        "target": '.narrow-td',
-        "aoColumns": [
-            null,
-            null,
-            { "sType": "date-eu" },
-            null  
-        ],
-        "ordering": true,
-        "responsive": true,
-        "pageLength": 5,
-        "order": [ 2, 'desc' ],
-        "fnDrawCallback": function( oSettings ) {
-          if(oSettings.fnRecordsTotal() <= 5) {
-            $('.docs-show-all').hide();
-          }
-        }
-      });
-
-      console.log($('.' + wrapperId).find('.docs-show-all'));
-
-      // On search check if there are more or less than 5 results , hide or show Show all button
-      $('.' + wrapperId).on( 'search.dt', function () {
-        if(table.page.info().recordsDisplay < 5) {
-          $('.docs-show-all').hide();
-        } else {
-          $('.docs-show-all').show();
-        }
-      });
-
-      $('.dataTables_filter input').attr("placeholder", "Zoeken");
-
-      //Add a div to display a reset button (initially hidden with css)
-      $(".dataTables_filter").append("<div class='clear_button'></div>"); 
-
-      $('.dataTables_wrapper .dataTables_filter input').change(function(){
-
-        //show clear-button if input is not empty
-        if( !$(this).val()){
-            $(".clear_button").hide();
-        } else {
-            $(".clear_button").show();
-        }
-      });
-
-      $('.docs-show-all').on('click', function(){ 
-        if($(this).hasClass('changed')) {
-          $(this).removeClass('changed');
-          table.page.len(5).draw();
-        } else {
-          $('.docs-show-all').toggleClass('changed');
-          table.page.len(-1).draw();
-        }
-      });
-
-       $('.clear_button').on('click', function(){ 
-        $('.dataTables_wrapper .dataTables_filter input').val('');
-        $('.dataTables_wrapper .dataTables_filter input').trigger('keyup.DT');
-        $(".clear_button").hide();
-      });
-
+      this.sortTable('.sortabletable', '.sortable', 2);
     },
 
     sortTable: function (table_class, div_class, sortDefColumn) {
@@ -102,15 +34,17 @@
         "pageLength": 5,
         "order": [ sortDefColumn, 'desc' ],
         "fnDrawCallback": function( oSettings ) {
-          console.log(oSettings);
+          // Callback function, check if there are more or less than 5 queries and show/hide the Show all button
           if(oSettings.fnRecordsTotal() <= 5) {
+            console.log(oSettings.fnRecordsTotal() );
             $('.docs-show-all'+div_class).hide();
           }
         }
       });
+
       var wrapperId = $(table_class).attr('id')+'_wrapper';
     
-      // On search check if there are more or less than 5 results , hide or show Show all button
+      // On search, check if there are more or less than 5 results , hide or show Show all button
       $(table_class).on( 'search.dt', function () {
         if(table.page.info().recordsDisplay < 5) {
           $('.docs-show-all'+div_class).hide();
@@ -119,27 +53,10 @@
         }
       });
 
+      // Adds placeholder to the search bar
       $('#' + wrapperId + ' .dataTables_filter input').attr("placeholder", "Zoeken");
    
-      //Add a div to display a reset button (initially hidden with css)
-      $('#' + wrapperId + ' .dataTables_filter').append("<div class='clear_button'></div>"); 
-    
-      $('#' + wrapperId + ' .dataTables_filter input').change(function(){
-        //show clear-button if input is not empty
-        if(!$("#" + wrapperId + " .dataTables_filter > input").val()){
-            $("#" + wrapperId + " .clear_button").hide();
-        } else {
-            $("#" + wrapperId + " .clear_button").show();
-        }
-      });
-    
-      $('#' + wrapperId + ' .clear_button').on('click', function(){ 
-         $('#' + wrapperId + ' .dataTables_filter input').val('');
-        $('#' + wrapperId + ' .dataTables_filter input').trigger('keyup.DT');
-        $('#' + wrapperId + ' .clear_button').hide();
-      });
-    
-      $(div_class).on('click', function(){ 
+      $('.docs-show-all'+div_class).on('click', function(){ 
         if($(this).hasClass('changed')) {
           $(this).removeClass('changed');
           table.page.len(5).draw();
