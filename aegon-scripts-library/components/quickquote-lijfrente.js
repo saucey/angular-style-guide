d214caed0c4cdbd43c9788d6a7688773ec2b6c55
Rik Toetenel <riktoet@gmail.com>
2015-12-31 11:21:13 +0100

    Cleaned up code after consideration of Robbert

--- aegon-scripts-library/components/quickquote-lijfrente.js
+++ aegon-scripts-library/components/quickquote-lijfrente.js
@@ -7,27 +7,25 @@
   // Add new item to public Drupal object
   Drupal.behaviors.quickquote = {
     attach: function () {
+      Drupal.behaviors.quickquote.slider("#ammount-slider","#ammount-input",500,0,5000,100,"€");
+      Drupal.behaviors.quickquote.slider("#time-slider","#time-input",10,0,30,1,"");      
+    },
+    slider: function(sliderClass,inputClass,sliderValue,sliderMin,sliderMax,sliderStep, Currenty) {
       Drupal.behaviors.tooltip.activate(".quickquote");
-      function quickquoteSlider(sliderClass,inputClass,sliderValue,sliderMin,sliderMax,sliderStep, Currenty) {
-        $(sliderClass).slider({
-          range: "max",
-          value:sliderValue,
-          min: sliderMin,
-          max: sliderMax,
-          step: sliderStep,
-          slide: function( event, ui ) {
-            $(inputClass).val( Currenty + ui.value );
-          }
-        });
-        $(inputClass).val( Currenty + $(sliderClass).slider( "value" ) );
-        $(inputClass).keyup(function() {
-          $(sliderClass).slider("value" , $(this).val().replace(/\u20ac/g, ''))
-        });
-      }
-
-      quickquoteSlider("#ammount-slider","#ammount-input",500,0,5000,100,"€");
-      quickquoteSlider("#time-slider","#time-input",10,0,30,1,"");
-      
+      $(sliderClass).slider({
+        range: "max",
+        value:sliderValue,
+        min: sliderMin,
+        max: sliderMax,
+        step: sliderStep,
+        slide: function( event, ui ) {
+          $(inputClass).val( Currenty + ui.value );
+        }
+      });
+      $(inputClass).val( Currenty + $(sliderClass).slider( "value" ) );
+      $(inputClass).keyup(function() {
+        $(sliderClass).slider("value" , $(this).val().replace(/\u20ac/g, ''))
+      });
     }
   };
-})(jQuery);
+})(jQuery); 
