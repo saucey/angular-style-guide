a7955ecc9341110db6698b688d51346e4bd2436d
Rik Toetenel <riktoet@gmail.com>
2016-01-08 17:00:27 +0100

    Calculation completed, please help me with keeping the slider dynamic.

--- aegon-scripts-library/components/slider.js
+++ aegon-scripts-library/components/slider.js
@@ -1,27 +1,29 @@
-/**
- * jQuery UI Slider
- */
-(function($) {
-  'use strict';
+// /**
+//  * jQuery UI Slider
+//  */
+// (function($) {
+//   'use strict';
 
-  // Add new item to public Drupal object
-  Drupal.behaviors.slider = {
-    activate: function(sliderClass,inputClass,sliderValue,sliderMin,sliderMax,sliderStep,Currenty) {
-      Drupal.behaviors.tooltip.activate(".quickquote");
-      $(sliderClass).slider({
-        range: "max",
-        value:sliderValue,
-        min: sliderMin,
-        max: sliderMax,
-        step: sliderStep,
-        slide: function( event, ui ) {
-          $(inputClass).val( Currenty + ui.value );
-        }
-      });
-      $(inputClass).val( Currenty + $(sliderClass).slider( "value" ) );
-      $(inputClass).keyup(function() {
-        $(sliderClass).slider("value" , $(this).val().replace(Currenty, ''));
-      });
-    }
-  };
-})(jQuery); 
+//   // Add new item to public Drupal object
+//   Drupal.behaviors.slider = {
+//     activate: function(sliderClass,inputClass,sliderValue,sliderMin,sliderMax,sliderStep,Currenty) {
+//       Drupal.behaviors.tooltip.activate(".quickquote");
+//       $(sliderClass).slider({
+//         range: "max",
+//         value:sliderValue,
+//         min: sliderMin,
+//         max: sliderMax,
+//         step: sliderStep,
+//         slide: function( event, ui ) {
+//           $(inputClass).val( Currenty + ui.value );
+//           console.log("I'm changed");
+//           Drupal.behaviors.quickquote.lijfrenteCalculation(3.05, 35000, 5, "#payment-calculated", "#interest-calculated", "€");
+//         }
+//       });
+//       $(inputClass).val( Currenty + $(sliderClass).slider("value"));
+//       $(inputClass).keyup(function() {
+//         $(sliderClass).slider("value" , $(this).val().replace(Currenty, ''));
+//       });
+//     }
+//   };
+// })(jQuery); 
