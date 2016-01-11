47b0c296ee2c10c3b03e3b5df89fff8f61a40d89
Rik Toetenel <riktoet@gmail.com>
2016-01-11 17:09:08 +0100

    Fixed typos and commented on slider.js

--- aegon-scripts-library/components/quickquote-lijfrente.js
+++ aegon-scripts-library/components/quickquote-lijfrente.js
@@ -10,7 +10,7 @@
       Drupal.behaviors.quickquote.slider("#amount-slider","#amount-input",1000,0,100000,100,"€");
       Drupal.behaviors.quickquote.slider("#time-slider","#time-input",10,0,30,1,"");
     },
-    slider: function(sliderClass,inputClass,sliderValue,sliderMin,sliderMax,sliderStep,Currenty) {
+    slider: function(sliderClass,inputClass,sliderValue,sliderMin,sliderMax,sliderStep,Currency) {
       Drupal.behaviors.tooltip.activate(".quickquote");
       $(sliderClass).slider({
         range: "max",
@@ -19,16 +19,16 @@
         max: sliderMax,
         step: sliderStep,
         slide: function( event, ui ) {
-          $(inputClass).val( Currenty + ui.value );
+          $(inputClass).val( Currency + ui.value );
           Drupal.behaviors.quickquote.lijfrenteCalculation(3.05, "#payment-calculated", "#interest-calculated", "€");
         }
       });
-      $(inputClass).val( Currenty + $(sliderClass).slider("value"));
+      $(inputClass).val( Currency + $(sliderClass).slider("value"));
       $(inputClass).keyup(function() {
-        $(sliderClass).slider("value" , $(this).val().replace(Currenty, ''));
+        $(sliderClass).slider("value" , $(this).val().replace(Currency, ''));
       });
     },
-    lijfrenteCalculation: function(interest,paymentClass,interestClass,Currenty) {
+    lijfrenteCalculation: function(interest,paymentClass,interestClass,Currency) {
       var roundingDecimals = function(input, decimals) {
         return Math.round(input * Math.pow(10, decimals)) / Math.pow(10, decimals);
       };
@@ -41,11 +41,11 @@
       var interestPerMonth = roundingDecimals(Math.pow(1 + (interest / 100), 1 / 12) - 1, 6),
           formulaPart1 = roundingDecimals(1 / Math.pow(1 + interestPerMonth, months), 6),
           formulaComplete = roundingDecimals((1 - formulaPart1) / interestPerMonth, 3),  
-          montlyPayment = roundingDecimals(money / formulaComplete, 2);
+          monthlyPayment = roundingDecimals(money / formulaComplete, 2);
       if (money == 0 || time == 0){
-        montlyPayment = 0;
+        monthlyPayment = 0;
       }
-      $(paymentClass).text( Currenty + montlyPayment);
+      $(paymentClass).text(Currency + monthlyPayment);
     },
   };
 })(jQuery); 
