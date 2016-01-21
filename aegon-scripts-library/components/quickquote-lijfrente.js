4e86a49eb8308200636fcf7755e02f6c3f0e2296
Rik Toetenel <riktoet@gmail.com>
2016-01-21 16:32:10 +0100

    Changed rounding decimals to exactly 2 decimals

--- aegon-scripts-library/components/quickquote-lijfrente.js
+++ aegon-scripts-library/components/quickquote-lijfrente.js
@@ -37,11 +37,11 @@
       if (isNaN(money) || months === 0 || isNaN(months)) {
         return 0;
       }
-      var interestPerMonth = round(Math.pow(1 + (interest[duration] / 100), 1 / 12) - 1, 6);
-      var formulaPart1 = round(1 / Math.pow(1 + interestPerMonth, months), 6);
-      var formulaComplete = round((1 - formulaPart1) / interestPerMonth, 3);
-      var monthlyPayment = round(money / formulaComplete, 2);
-      var totalInterest = round(((monthlyPayment * months) - money), 2).toFixed(2);
+      var interestPerMonth = round(Math.pow(1 + (interest[duration] / 100), 1 / 12) - 1, 6),
+          formulaPart1 = round(1 / Math.pow(1 + interestPerMonth, months), 6),
+          formulaComplete = round((1 - formulaPart1) / interestPerMonth, 3),
+          monthlyPayment = round(money / formulaComplete, 2),
+          totalInterest = round(((monthlyPayment * months) - money), 2).toFixed(2);
       $(paymentClass).text( Currency + monthlyPayment);
       $(interestClass).text( Currency + totalInterest);
     },
