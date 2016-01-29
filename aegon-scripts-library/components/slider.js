a8bc41f52185e22087431ac7b16637b667e82bff
Rik Toetenel <riktoet@gmail.com>
2016-01-29 15:12:57 +0100

    Temp Commit

--- aegon-scripts-library/components/slider.js
+++ aegon-scripts-library/components/slider.js
@@ -1,26 +1,18 @@
 // /**
-//  * jQuery UI Slider.
+//  * jQuery UI Slider. This function is being commented out because the slider should be made generic from /components/quickquote-lijfrente.js
 //  */
 (function($) {
   'use strict';
 
   // Add new item to public Drupal object
   Drupal.behaviors.slider = {
-    activate: function(sliderClass,inputClass,sliderValue,sliderMin,sliderMax,sliderStep, options) {
+    activate: function(sliderClass,inputClass,sliderValue,sliderMin,sliderMax,sliderStep,currency, options) {
 
       // Creates dot between thousands
-      function readableNumber(number,location) {
-        if (location === "slider") {
-          var sliderNumber = number.toLocaleString();
-          sliderNumber = sliderNumber.replace(/,/g, '.');
-          return sliderNumber;
-        }
-        else if (location === "input") {
-          console.log("in input function: " + number);
-          var inputNumber = number.replace(/(,00)|\./g, "");
-          console.log("in input function: " + inputNumber);
-          return inputNumber;
-        }
+      function readableNumber(number) {
+        var newNumber = number.toLocaleString();
+        newNumber = newNumber.replace(/,/g, '.');
+        return newNumber;
       }
 
       // Default options
@@ -31,7 +23,7 @@
         max: sliderMax,
         step: sliderStep,
         slide: function( event, ui ) {
-          $(inputClass).val(readableNumber(ui.value, "slider") );
+          $(inputClass).val( currency + readableNumber(ui.value) );
         }
       };
 
@@ -41,12 +33,9 @@
       // Init slider
       $(sliderClass).slider(settings);
 
-      $(inputClass).val($(sliderClass).slider("value"));
-      $(inputClass).change(function() {
-        var inputValue = $(this).val();
-        inputValue = readableNumber(inputValue, "input");
-        console.log ("in change function: " + inputValue);
-        $(sliderClass).slider("value" , $(this).val().replace(/(,00)|\./g, ""));
+      $(inputClass).val( currency + $(sliderClass).slider("value"));
+      $(inputClass).keyup(function() {
+        $(sliderClass).slider("value" , $(this).val().replace(currency, ''));
       });
     }
   };
