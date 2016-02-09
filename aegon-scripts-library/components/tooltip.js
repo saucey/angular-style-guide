56f6a74c7736fe8f214e1c6ec82261744398e88d
Sebastian Lopez <mauricio.lopez@aegon.nl>
2016-02-09 12:50:05 +0100

    Update in the tooltip code to avoid showing the default title browser behavior.

--- aegon-scripts-library/components/tooltip.js
+++ aegon-scripts-library/components/tooltip.js
@@ -12,16 +12,16 @@
     },
     attached: false,
     activate: function (selector, pos) {
-
       var isTouch = "ontouchstart" in window || navigator.msMaxTouchPoints,
       openTooltip = function(ele, pos){
         var $this = ele,
-            $title = $this.attr('title'),
+            $title = $this.attr('data-title') !== undefined ? $this.attr('data-title') : $this.attr('title'),
             $touch = $this.attr('data-touch') === "true" ? true : false,
             $pos = $this.attr('data-pos'),
             posRE = new RegExp(/^(top|bottom|left|right)$/);
         
-        if ($title && $title !== " " && $title.length > 0) { //the temporary content has 2B " ", since "" will set display to "none" according to stylesheet definition, 
+        if ($title && $title !== "" && $title.length > 0) {
+
           $(".help.dialog").remove();
           // if the element has the attr data-pos
           if($pos !== 'undefined' && posRE.test($pos)){
@@ -36,7 +36,8 @@
           dialog.className = "help dialog " + pos;
           dialog.innerHTML = $title;
 
-          $this.attr('title', ' ');
+          $this.attr('title', '');
+          $this.attr('data-title', '');
 
           $(selector).append(dialog); //this has 2 happen b4 measurements of dialog are taken, otherwise they won't be initialized
           var $dialog = $(dialog),
@@ -64,24 +65,40 @@
           $dialog.offset(offset);
 
           $(document).click(function () {
-            $this.attr('title', dialog.innerHTML);
+            $this.attr('data-title', dialog.innerHTML);
             $(".dialog").remove();
           });
 
           $this.on('mouseleave', function(){
-            $this.attr('title', dialog.innerHTML);
+            $this.attr('data-title', dialog.innerHTML);
             $dialog.remove();
           });
 
           if (isTouch && $touch){
             $this.on('click', function (e) {
               e.stopPropagation();
-              $this.attr('title', dialog.innerHTML);
+              $this.attr('data-title', dialog.innerHTML);
               $dialog.remove();
             });      
           }        
         }
+        if($title === '' || $title === undefined){
+          $this.hide();
+        }
       };
+      // initial scan for help items to hide them 
+      // if they don't have either data-title or title
+      if($(selector).find(".help").length > 0){
+        $(selector).find(".help").each(function(){
+          var $helpEle = $(this),
+            $title = ($helpEle.attr('title') !== undefined && $helpEle.attr('title').trim() !== ''),
+            $dataTitle = ($helpEle.attr('data-title') !== undefined && $helpEle.attr('data-title').trim() !== '');
+
+          if($title === false && $dataTitle === false){
+            $helpEle.hide();
+          }
+        });
+      }
 
       $(selector + " .help").on('mouseenter', function () {
         openTooltip($(this), pos);
