467606f930e1583a38fc85690f6ed628ef46c2a1
Sebastian Lopez <mauricio.lopez@aegon.nl>
2016-02-10 10:45:25 +0100

    Remove the verification as datatables always adds an id to the table

--- aegon-scripts-library/components/table.js
+++ aegon-scripts-library/components/table.js
@@ -59,12 +59,8 @@
               });
             } 
 
-          // Set placeholder for searchbox 
-          that.parent().find('.dataTables_filter input').attr("placeholder", "Zoeken");
-
-          if(typeof that.attr('id') === 'string'){
-            that.parent().find('.dataTables_filter input').attr('id', 'search_'+that.attr('id'));
-          }
+          // Set placeholder and id for searchbox 
+          that.parent().find('.dataTables_filter input').attr("placeholder", "Zoeken").attr('id', 'search_'+that.attr('id'));
         }
       };
 
