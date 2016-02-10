0f9c2eee541c3b33a390fdb8a22c5929bb7c87b4
Sebastian Lopez <mauricio.lopez@aegon.nl>
2016-02-10 10:38:42 +0100

    Added verification that the id in the form exists

--- aegon-scripts-library/components/table.js
+++ aegon-scripts-library/components/table.js
@@ -60,7 +60,11 @@
             } 
 
           // Set placeholder for searchbox 
-          that.parent().find('.dataTables_filter input').attr("placeholder", "Zoeken").attr('id', 'search_'+that.attr('id'));
+          that.parent().find('.dataTables_filter input').attr("placeholder", "Zoeken");
+
+          if(typeof that.attr('id') === 'string'){
+            that.parent().find('.dataTables_filter input').attr('id', 'search_'+that.attr('id'));
+          }
         }
       };
 
