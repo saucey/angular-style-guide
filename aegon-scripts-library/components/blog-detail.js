99a8de253173eb0e12fc24b2d4b405903e08d85f
Rik Toetenel <riktoet@gmail.com>
2016-02-12 16:45:52 +0100

    Changed behaviour name and added on valid method

--- aegon-scripts-library/components/blog-detail.js
+++ aegon-scripts-library/components/blog-detail.js
@@ -1,22 +1,23 @@
 /**
- * Blog form validatoin
+ * Comment form validatoin
  */
 
  (function($) {
    'use strict';
 
    // Add new item to public Drupal object
-   Drupal.behaviors.blog = {
+   Drupal.behaviors.comment_form = {
      attach: function () {
        var reaction = $("#comment-form");
        reaction.validVal({
          form: {
            onInvalid: function() {
-           reaction.find($(".error")).show();
-       }
-     }
-
-
+            reaction.find($(".error")).show();
+           },
+           onValid: function() {
+             reaction.find($(".error")).hide();
+           }
+         }
      });
 
      }
