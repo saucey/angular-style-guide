6b676429e6021e791999ca5583bbac5e492ef020
Rik Toetenel <i.m@awesome.yeah>
2016-03-04 14:56:15 +0100

    Added sliders

--- aegon-scripts-library/test/quickquote.spec.js
+++ aegon-scripts-library/test/quickquote.spec.js
@@ -2,16 +2,30 @@
   'use strict';
 
   describe('quickquote lijfrente uitkeren', function () {
-    var qq = Drupal.behaviors.quickquoteLijfrente;
+    var qqLijfrente = Drupal.behaviors.quickquoteLijfrente;
 
     describe('lijfrente berekenen', function () {
       it('should return 0 for invalid input values', function() {
-        expect(qq.calculateMonthlyPayment(20, 0)).toEqual(0);
-        expect(qq.calculateMonthlyPayment('')).toEqual(0);
+        expect(qqLijfrente.calculateMonthlyPayment(20, 0)).toEqual(0);
+        expect(qqLijfrente.calculateMonthlyPayment('')).toEqual(0);
       });
-      it('should return some values (but will fail because these values are not correct)', function() {
-        expect(qq.calculateMonthlyPayment(10000, 5)).toEqual("174,34");
+      it('should return the proper values after calculation (but will fail because these values are not correct)', function() {
+        expect(qqLijfrente.calculateMonthlyPayment(10000, 5)).toEqual("174,34");
       });
     });
   });
+
+  //describe('quickquote lijfrente uitkeren', function () {
+  //  var qqLijfrenteSparen = Drupal.behaviors.quickquoteLijfrenteSparen;
+  //
+  //  describe('lijfrente berekenen', function () {
+  //    it('should return 0 for invalid input values', function() {
+  //      expect(qqLijfrenteSparen.calculateMonthlyPayment(20, 0)).toEqual(0);
+  //      expect(qqLijfrenteSparen.calculateMonthlyPayment('')).toEqual(0);
+  //    });
+  //    it('should return the proper values after calculation (but will fail because these values are not correct)', function() {
+  //      expect(qqLijfrenteSparen.calculateMonthlyPayment(10000, 5)).toEqual("174,34");
+  //    });
+  //  });
+  //});
 })();
