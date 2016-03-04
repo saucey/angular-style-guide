b9b9274edfb5e884121ef547084e60d29b1aa01d
Rik Toetenel <i.m@awesome.yeah>
2016-03-04 13:27:43 +0100

    Succesful jasmine/karma/phantomjs test

--- aegon-scripts-library/test/some.spec.js
+++ aegon-scripts-library/test/some.spec.js
@@ -1,37 +1,16 @@
 (function () {
   'use strict';
 
-  var calculator = {
-    add: function (arg1, arg2) {
-      return arg1 + arg2;
-    }
-  };
-
-  describe('calculator', function() {
-    describe('add()', function() {
-      it('should add 2 numbers togoether', function() {
-        // assertions here
-        expect(calculator.add(1, 4)).toEqual(5);
-      });
-    });
-  });
-
-  //var Drupal = require('../vendor/drupal_misc/drupal.js');
-  var qq = require('../components/quickquote-lijfrente.js');
-
   describe('quickquote lijfrente uitkeren', function () {
     var qq = Drupal.behaviors.quickquoteLijfrente;
 
     describe('lijfrente berekenen', function () {
       it('should return 0 for invalid input values', function() {
-        expect(qq.calculateMonthlyPayment('foo', 'bar')).toEqual(0);
         expect(qq.calculateMonthlyPayment(20, 0)).toEqual(0);
         expect(qq.calculateMonthlyPayment('')).toEqual(0);
       });
       it('should return some values (but will fail because these values are not correct)', function() {
-        expect(qq.calculateMonthlyPayment(1, 2)).toEqual(9000);
-        expect(qq.calculateMonthlyPayment(3, 4)).toEqual(300);
-        expect(qq.calculateMonthlyPayment(5, 6)).toEqual(750);
+        expect(qq.calculateMonthlyPayment(10000, 5)).toEqual("174,34");
       });
     });
   });
