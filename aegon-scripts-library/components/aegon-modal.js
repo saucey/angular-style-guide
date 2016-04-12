def171e70f2e5610bf07cbd9c5fcce7543e1f4b4
Sebastian Lopez <mauricio.lopez@aegon.nl>
2016-04-12 16:32:50 +0200

    Reusable modal script

---
+++ aegon-scripts-library/components/aegon-modal.js
@@ -0,0 +1,171 @@
+/**
+ * AegonModal function declaration
+ *
+ * @param action (string)(optional): 'open' (default) || 'close'
+ * @param options (object): options for the modal
+ * @param callback (function)(optional): what to do after the modal is open
+ */
+// Closure with jQuery support
+(function(Drupal, $) {
+  'use strict';
+
+  Drupal.aegonModal = function(action, options, callback) {
+    var modal,
+    defaults = {
+      title: '', // @string: title text, no HTML
+      body: '', // @string: HTML for the body
+      close: true, // @boolean: Add close button
+      ajax: false, // @boolean: whether to add the loader or not
+      ajaxText: '', // @string: Text to show while waiting for ajax
+      overlay: true // @boolean: dark overlay behind the modal
+    };
+
+    // if the second param is the callback
+    if(typeof options === 'function') {
+      callback = options;
+      options = null;
+    }
+
+    // if only the options are passed 
+    if(typeof action === 'object') {
+      options = action;
+      // Will be the default action.
+      action = null;
+    }
+
+    if(action === 'open' || action === 'update') {
+      // options are mandatory for open and update
+      if(typeof options === 'undefined' || typeof options === null) {
+        throw new Error('No options were passed');
+      }
+    }
+
+    var settings = $.extend({}, defaults, options);
+    /*
+     * Creates the modal layout according to
+     * the settings
+     */
+    function createModal() {
+      // delete the modal
+      if(action !== 'update') {
+        closeModal();
+      }
+      // Layout setting.
+      var modalInner = '';
+
+      // Modal layout
+      if(typeof settings.title === 'string' && settings.title.trim() !== '') {
+        modalInner += '<h2>' + settings.title + '</h2>';
+      }
+      if(typeof settings.close === 'boolean' && settings.close) {
+        modalInner += '<a class="icon-uniE60D close-modal" data-close="modal"></a>';
+      }
+      if(typeof settings.body === 'string' && settings.body.trim() !== '') {
+        // all the HTML of the body is wrapped
+        modalInner += '<div class="modal-content">' + settings.body + '</div>';
+      }
+      // loader for ajax content
+      if(typeof settings.ajax === 'boolean' && settings.ajax) {
+        modalInner += '<div class="modal-loader"></div>';
+      }
+      if(typeof settings.ajaxText === 'string' && settings.ajaxText.trim() !== '') {
+        // all the HTML of the body is wrapped
+        modalInner += '<p class="ajax-text">' + settings.ajaxText + '</p>';
+      }
+
+      // Makes sure it contains something
+      if(modalInner !== '') {
+
+        var modalEle = document.createElement('DIV');
+        modalEle.className = 'aegon-modal visible';
+        modalEle.innerHTML = modalInner;
+
+        // Insert the modal
+        $('body').append(modalEle);
+
+        var $modal = $(modalEle);
+
+        positionModal();
+        // Add click event to close modal
+        $(document).on('click', '.aegon-modal .close-modal', function() {
+          Drupal.aegonModal('close');
+        });
+        // overlay layout
+        if(settings.overlay) {
+          if(! $(document).find('.aegon-modal-overlay visible').length) {
+            $('body').append('<div class="aegon-modal-overlay lightbox"></div>');
+          }
+        } else {
+          if($(document).find('.aegon-modal-overlay').length) {
+            $('.aegon-modal-overlay').remove();
+          }
+        }
+
+        return $modal;
+
+      } else {
+        var e = new Error('No options were passed for the modal content'); 
+        throw e;
+      }
+
+    }
+
+    function positionModal() {
+      var $modal = $(document).find('.aegon-modal'),
+      $window = $(window);
+
+      if($modal.length){
+        // Reset CSS
+        $modal.css({'bottom':'auto', 'overflow-y':'visible', 'margin-left': - ($modal.outerWidth() / 2) });
+
+        if($window.height() < $modal.outerHeight()) {
+          $modal.css({'top': 20, 'bottom': 20, 'overflow-y':'auto'});
+        }else{
+          $modal.css({
+            top: (($window.height() - $modal.outerHeight()) / 2)
+          });
+        }
+      }
+      // return something
+      return true;
+    }
+    /*
+     * Remove the modal and the overlay
+     * elements from DOM
+     */
+    function closeModal() {
+      $(document).find('.aegon-modal, .aegon-modal-overlay').remove();
+
+      return true;
+    }
+
+    // Actions.
+    switch( action ) {
+      case 'open':
+      case 'update':
+        modal = createModal();
+        break;
+      case 'position':
+        modal = positionModal();
+        break;
+      case 'close':
+        modal = closeModal();
+        break;
+      default:
+        modal = createModal();
+        break;
+    }
+
+    /*
+     * Return the element for further manipulation
+     * or true when closed.
+     */
+
+    return modal;
+  };
+
+  $(window).on('resize', function() {
+    Drupal.aegonModal('position');
+  });
+
+})(Drupal, jQuery);
