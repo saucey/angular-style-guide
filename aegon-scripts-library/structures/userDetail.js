2af548a13c0cd715de5f44377092ef8a9f5dd422
Sebastian Lopez <mauricio.lopez@aegon.nl>
2016-05-17 15:42:39 +0200

    Conditional for adding class depending on the checkbox status

--- aegon-scripts-library/structures/userDetail.js
+++ aegon-scripts-library/structures/userDetail.js
@@ -314,7 +314,11 @@
       $template.find('a.user_detail_widget_mijnaegon_link').attr(
         'href', mijnaegonPathLink);
       $template.find('#UDW_dd_show').on('change', function() {
-        $(this).prop('checked') === true ? $template.addClass('open') : $template.removeClass('open');
+        if($(this).prop('checked') === true) {
+          $template.addClass('open');
+        }else{
+          $template.removeClass('open');
+        }
       });
       // Exception in case data.lastAccess is empty
       if (data.lastAccess === false) {
