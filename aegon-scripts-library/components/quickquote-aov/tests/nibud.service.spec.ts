69c01ee9f0bfe41a02d751631f38e7e00f80dc3a
Robbert van den Bogerd <rvdbogerd@bliq.net>
2016-05-18 18:06:40 +0200

    wip: change some deps

--- aegon-scripts-library/components/quickquote-aov/tests/nibud.service.spec.ts
+++ aegon-scripts-library/components/quickquote-aov/tests/nibud.service.spec.ts
@@ -1,9 +1,20 @@
-//describe('TestService', () => {
+// import {NibudService} from '../nibud.service';
+// import {HTTP_PROVIDERS, Http} from 'angular2/http';
+// import {beforeEachProviders, beforeEach, provide} from "angular2/testing";
+// import beforeEach = testing.beforeEach;
 //
-//  beforeEachProviders(() => [TestService]);
+// describe('NibudService', () => {
 //
-//  it('should have name property set', inject([TestService], (testService: TestService) => {
-//    expect(testService.name).toBe('Injected Service');
-//  }));
+//   beforeEachProviders(() => [
+//       provide(HTTP_PROVIDERS),
+//       NibudService
+//   ]);
+//   beforeEach(function () {
+//      this.testService = new NibudService(Http);
+//   });
 //
-//});
+//   it('should have name property set', inject([TestService], (testService: TestService) => {
+//     expect(testService.name).toBe('Injected Service');
+//   }));
+//
+// });
