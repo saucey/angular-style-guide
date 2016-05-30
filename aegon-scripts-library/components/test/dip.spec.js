5fa569d95f2ddb291b1e6f2842188e6b956059c2
Robbert van den Bogerd <rvdbogerd@bliq.net>
2016-05-30 14:57:36 +0200

    first attempt to use ts tests

---
+++ aegon-scripts-library/components/test/dip.spec.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    describe('quickquote DIP', function () {
+        it('should do something really cool', function() {
+            expect(true).toBeTruthy();
+        });
+    });
+})();
