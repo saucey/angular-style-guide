e218257d9c7a365911a576a0841228b152454d2c
Sebastian Lopez <mauricio.lopez@aegon.nl>
2016-06-07 09:05:24 +0200

    Enabled prod mode

--- aegon-scripts-library/components/quickquote-boeterente/main.ts
+++ aegon-scripts-library/components/quickquote-boeterente/main.ts
@@ -2,5 +2,5 @@ import {enableProdMode} from 'angular2/core'
 import {bootstrap}    from 'angular2/platform/browser';
 import {QuickQuoteBoeterenteComponent} from './quickquote-boeterente.component';
 
-//enableProdMode();
+enableProdMode();
 bootstrap(QuickQuoteBoeterenteComponent);
