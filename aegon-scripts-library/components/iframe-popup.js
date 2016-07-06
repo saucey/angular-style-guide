440c391edc1c923d0e93ff5c268f6ef9db8af7fd
Michiel van der Graaf <michiel@dynora.nl>
2016-07-06 14:25:33 +0200

    popup element generator added

--- aegon-scripts-library/components/iframe-popup.js
+++ aegon-scripts-library/components/iframe-popup.js
@@ -8,18 +8,51 @@
   // Add new item to public Drupal object
   Drupal.behaviors.iframePopup = {
     attach: function () {
-    var styleElm = document.createElement("STYLE");
-    styleElm.innerHTML = "html, body { overflow: hidden; height: 100%; }";
+      var styleElm = document.createElement("style");
+      styleElm.innerHTML = "html, body { overflow: hidden; height: 100%; }";
 
+      // The popup element holder is used to add and remove the popup from the body.
+      var popupElm;
+
+      var removeIframePopup = function () {
+        document.head.removeChild(styleElm);
+        document.body.removeChild(popupElm);
+        popupElm = null;
+      };
+
+      var generatePopupElm = function (url) {
+        // Generate the popup element. When added to the dom it will start loading the iframe data.
+        popupElm = document.createElement("div");
+        popupElm.className = "iframe-popup popup-show";
+        popupElm.innerHTML = '<div class="popup-container">' +
+          '<a class="close-popup close-iframe-popup"></a>' +
+            '<div class="popup-content">' +
+              '<iframe src="' + url + '"></iframe>' +
+            '</div>' +
+          '</div>';
+      };
+
+      var showIframePopup = function (url) {
+        // Generate popup element.
+        generatePopupElm(url);
+
+        // Add the elements to the dom.
+        document.body.appendChild(popupElm);
+        document.head.appendChild(styleElm);
+
+        // Bind close event to the added iframe popup close button.
+        $('.close-iframe-popup').on('click', function () {
+          removeIframePopup();
+        });
+      };
+      // Expose this function to call it from Drupal.behaviors.
+      Drupal.behaviors.iframePopup.showIframePopup = showIframePopup;
+
+      // Find buttons or links that need to open the iframe popup.
       $('.show-iframe-popup').on('click', function () {
-        $(".iframe-popup").addClass("popup-show");
-        document.body.appendChild(styleElm);
+        showIframePopup();
       });
 
-      $('.close-iframe-popup').on('click', function () {
-        $(".iframe-popup").removeClass("popup-show");
-        document.body.removeChild(styleElm);
-      });
     },
     attached: false
   };
