0891b61f90d89e34cbcb98040701a7431e6e307d
Antonio D'Angelo <antonio.dangelo@hcl.com>
2016-08-08 16:50:45 +0200

    CXDEV-2642_AOVQuote | aanvragen-button correct url added

--- aegon-scripts-library/components/aov-quote/aov-quote.component.ts
+++ aegon-scripts-library/components/aov-quote/aov-quote.component.ts
@@ -216,7 +216,7 @@ var templateElem = (<HTMLTextAreaElement>document.querySelector('#aovQuoteTempla
             </div>
             
             <label class="label span12 aanvragen-label dark-blue" for="aanvragen">Wilt u een vrijblijvend adviesgesprek?</label>
-            <button class="arrow span8 orange aanvragen-button" type="button">Adviesgesprek aanvragen</button>
+            <a class="arrow span8 orange aanvragen-button button" href="https://www.aegon.nl/zakelijk/inkomensverzekeringen/arbeidsongeschiktheidsverzekering/afspraak-arbeidsongeschiktheidsverzekering-advies">Neem contact met mij op</a>
           </div>
         </div>
       </div>
