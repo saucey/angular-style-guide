7b8c5c4770de1f5e44939d442deb85743246e3bf
Antonio D'Angelo <antonio.dangelo@hcl.com>
2016-08-19 13:35:53 +0200

    CXDEV-2901/storage-library | Refactory: removed getInstance

--- aegon-scripts-library/modules/clientStorage.js
+++ aegon-scripts-library/modules/clientStorage.js
@@ -2,9 +2,8 @@
  * 
  * Local storage wrapper: Basically is a Singleton and when instanciated, 
  * will return three different objects for Session, Local and Cookie storage.  
- * Ex. cs = clienteStorage.getinstance();
- *       cs.session.setItem(key, value);
- *       cs.session.getItem(key);
+ * Ex. clienteStorage.session.setItem(key, value);
+ *       clienteStorage.session.getItem(key);
  * The library also provide options to encode the data based on Base64.
  *
  * Author: Antonio D'Angelo, antonio.dangelo@hcl.com
@@ -12,19 +11,8 @@
  */
 
 clientStorage = (function() {
-    var instance,
-        // Create Base64 Object - IE9 support
-        Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}},
+    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}},
         base64Pattern = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/;
-
-    return {
-        getInstance: function() {
-            if(!instance) {
-                instance = init();
-            }
-            return instance;
-        }
-    };
 
     function isEncoded(str) {
         return base64Pattern.test(str);
@@ -139,62 +127,60 @@ clientStorage = (function() {
         }
     }
     
-    function init() {
-        return {
-            "session": {
-                "length": sessionStorage.length,
-                "key": function(_key) {
-                    return sessionStorage.key(_key);
-                },
-                "getItem": function(_keyName) {
-                    return innerGetItem(sessionStorage, _keyName);
-                },
-                "setItem": function(_key, _value, _options) {
-                    return innerSetItem(sessionStorage, _key, _value, _options)
-                },
-                "removeItem": function(_key) {
-                    return innerRemoveItem(sessionStorage, _key);
-                },
-                "clearStorage": function() {
-                    return innerClear(sessionStorage);
-                }
+    return {
+        "session": {
+            "length": sessionStorage.length,
+            "key": function(_key) {
+                return sessionStorage.key(_key);
+            },
+            "getItem": function(_keyName) {
+                return innerGetItem(sessionStorage, _keyName);
+            },
+            "setItem": function(_key, _value, _options) {
+                return innerSetItem(sessionStorage, _key, _value, _options)
+            },
+            "removeItem": function(_key) {
+                return innerRemoveItem(sessionStorage, _key);
+            },
+            "clearStorage": function() {
+                return innerClear(sessionStorage);
+            }
 
+        },
+        "local": {
+            "length": sessionStorage.length,
+            "key": function(_key) {
+                return localStorage.key(_key);
             },
-            "local": {
-                "length": sessionStorage.length,
-                "key": function(_key) {
-                    return localStorage.key(_key);
-                },
-                "getItem": function(_keyName) {
-                    return innerGetItem(localStorage, _keyName);
-                },
-                "setItem": function(_key, _value, _options) {
-                    return innerSetItem(localStorage, _key, _value, _options)
-                },
-                "removeItem": function(_key) {
-                    return innerRemoveItem(localStorage, _key);
-                },
-                "clearStorage": function() {
-                    return innerClear(localStorage);
-                }
+            "getItem": function(_keyName) {
+                return innerGetItem(localStorage, _keyName);
             },
-            "cookie": {
-                "length": document.cookie.split(";").length,
-                "key": function(_key) {
-                    return ((document.cookie.split(";") || []) [_key] || "=").split("=")[0];
-                },
-                "getItem": function(_keyName) {
-                    return getCookie(_keyName);
-                },
-                "setItem": function(_key, _value, options) {
-                    return setCookie(_key, _value, options);
-                },
-                "removeItem": function(_key) {
-                    return removeCookie(_key);
-                },
-                "clearStorage": function() {
-                    return deleteAllCookies();
-                }
+            "setItem": function(_key, _value, _options) {
+                return innerSetItem(localStorage, _key, _value, _options)
+            },
+            "removeItem": function(_key) {
+                return innerRemoveItem(localStorage, _key);
+            },
+            "clearStorage": function() {
+                return innerClear(localStorage);
+            }
+        },
+        "cookie": {
+            "length": document.cookie.split(";").length,
+            "key": function(_key) {
+                return ((document.cookie.split(";") || []) [_key] || "=").split("=")[0];
+            },
+            "getItem": function(_keyName) {
+                return getCookie(_keyName);
+            },
+            "setItem": function(_key, _value, options) {
+                return setCookie(_key, _value, options);
+            },
+            "removeItem": function(_key) {
+                return removeCookie(_key);
+            },
+            "clearStorage": function() {
+                return deleteAllCookies();
             }
         }
     }
