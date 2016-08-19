5c12562599f97c859c7970416f1387dfad934474
Antonio D'Angelo <antonio.dangelo@hcl.com>
2016-08-19 12:43:40 +0200

    CXDEV-2901/storage-library | Refactory: Final refactory

--- aegon-scripts-library/modules/clientStorage.js
+++ aegon-scripts-library/modules/clientStorage.js
@@ -13,60 +13,17 @@
 
 clientStorage = (function() {
     var instance,
-        options = {
-            // Encoding is available only for Local and Session storage
-            "encoding": true,
-            // Experimental: It will encode also the key
-            "fullEncoded" : false
-        },
         // Create Base64 Object - IE9 support
         Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}},
         base64Pattern = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/;
 
     return {
-        getInstance: function(opt) {
+        getInstance: function() {
             if(!instance) {
-                instance = init(opt);
+                instance = init();
             }
             return instance;
         }
-    };
-
-    function extend() {
-        // Variables
-        var extended = {};
-        var deep = false;
-        var i = 0;
-        var length = arguments.length;
-
-        // Check if a deep merge
-        if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
-            deep = arguments[0];
-            i++;
-        }
-
-        // Merge the object into the extended object
-        var merge = function (obj) {
-            for ( var prop in obj ) {
-                if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
-                    // If deep merge and property is an object, merge properties
-                    if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
-                        extended[prop] = extend( true, extended[prop], obj[prop] );
-                    } else {
-                        extended[prop] = obj[prop];
-                    }
-                }
-            }
-        };
-
-        // Loop through each object and conduct a merge
-        for ( ; i < length; i++ ) {
-            var obj = arguments[i];
-            merge(obj);
-        }
-
-        return extended;
-
     };
 
     function isEncoded(str) {
@@ -91,13 +48,7 @@ clientStorage = (function() {
     };
 
     function innerGetItem(storage, keyName) {
-        var key = keyName;
-
-        if(options.encoding && options.fullEncoded) {
-            key = encode(keyName);
-        }
-
-        var item = storage.getItem(key) || "";
+        var item = storage.getItem(keyName) || "";
 
         if(isEncoded(item)) {
             item = decode(item);
@@ -110,21 +61,19 @@ clientStorage = (function() {
         return item;
     }
 
-    function innerSetItem(storage, keyName, value, forceEncoding) {
-        var key = keyName,
-            item = value;
+    function innerSetItem(storage, keyName, value, options) {
+        var item = value;
+
+        options = options || {};
 
         item = JSON.stringify(item);
 
-        if(options.encoding || forceEncoding) {
+        if(options.encoding) {
             item = encode(item);
-            if(options.fullEncoded) {
-                key = encode(keyName);
-            }
         }
 
         try {
-            storage.setItem(key, item);
+            storage.setItem(keyName, item);
         } catch (e) {
             console.error('Quota exceeded!', e); //data wasn't successfully saved due to quota exceed so throw an error
             return false;
@@ -134,22 +83,16 @@ clientStorage = (function() {
     }
 
     function innerRemoveItem(storage, keyName) {
-        var key = keyName;
-
-        if(options.encoding && options.fullEncoded) {
-            key = encode(keyName);
-        }
-
-        storage.removeItem(key);
+        storage.removeItem(keyName);
     }
 
     function innerClear(storage) {
         storage.clear();
     }
 
-    function setCookie(cname, cvalue, exdays) {
+    function setCookie(cname, cvalue, options) {
         var d = new Date();
-        d.setTime(d.getTime() + ((Number.parseInt(exdays)||2000)*24*60*60*1000));
+        d.setTime(d.getTime() + ((Number.parseInt(options.exdays)||2000)*24*60*60*1000));
 
         var expires = "expires="+d.toUTCString();
         if(typeof cvalue ==='object') {
@@ -182,7 +125,7 @@ clientStorage = (function() {
     }
 
     function removeCookie(cname) {
-        setCookie(cname, "", -1);
+        setCookie(cname, "", {exdays:-1});
     }
 
     function deleteAllCookies() {
@@ -196,8 +139,7 @@ clientStorage = (function() {
         }
     }
     
-    function init(opt) {
-        options = extend(true, options, opt);
+    function init() {
         return {
             "session": {
                 "length": sessionStorage.length,
@@ -208,7 +150,7 @@ clientStorage = (function() {
                     return innerGetItem(sessionStorage, _keyName);
                 },
                 "setItem": function(_key, _value) {
-                    return innerSetItem(sessionStorage, _key, _value, _forceEncoding)
+                    return innerSetItem(sessionStorage, _key, _value, _options)
                 },
                 "removeItem": function(_key) {
                     return innerRemoveItem(sessionStorage, _key);
@@ -227,7 +169,7 @@ clientStorage = (function() {
                     return innerGetItem(localStorage, _keyName);
                 },
                 "setItem": function(_key, _value) {
-                    return innerSetItem(localStorage, _key, _value, _forceEncoding)
+                    return innerSetItem(localStorage, _key, _value, _options)
                 },
                 "removeItem": function(_key) {
                     return innerRemoveItem(localStorage, _key);
@@ -245,7 +187,7 @@ clientStorage = (function() {
                     return getCookie(_keyName);
                 },
                 "setItem": function(_key, _value, _exp_date) {
-                    return setCookie(_key, _value, _exp_date);
+                    return setCookie(_key, _value, {exdays: _exp_date});
                 },
                 "removeItem": function(_key) {
                     return removeCookie(_key);
