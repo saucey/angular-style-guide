0369efb27a8e79a59c2d93bbf6b381c267ed808a
Michiel van der Graaf <michiel@dynora.nl>
2016-08-19 16:50:28 +0200

    Moved all options to options file. Made aa-hint not display when text attribute has falsey value.

--- aegon-scripts-library/components/aov-quote/main.ts
+++
@@ -1,5 +0,0 @@
-import 'components/angular-bootstrap/main';
-import {bootstrap}    from 'angular2/platform/browser';
-import {AovQuoteComponent} from './aov-quote.component';
-
-bootstrap(AovQuoteComponent);
