2d905c86a2921a84e7ceb4a9fe4eea0538983a4b
Antonio D'Angelo <antonio.dangelo@hcl.com>
2016-08-19 14:32:40 +0200

    CXDEV-2901/storage-library | Refactory: lint errors

--- aegon-scripts-library/modules/clientStorage.js
+++ aegon-scripts-library/modules/clientStorage.js
@@ -10,15 +10,107 @@
 *
 */
 
-clientStorage = (function() {
+window.clientStorage = (function() {
   "use strict";
 
-  var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}},
-    base64Pattern = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/;
+  var Base64={
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    encode: function(e) {
+      var t = "",
+        n, r, i, s, o, u, a,
+        f = 0;
+        e = Base64._utf8_encode(e);
+        while (f < e.length) {
+          n = e.charCodeAt(f++);
+          r = e.charCodeAt(f++);
+          i = e.charCodeAt(f++);
+          s = n >> 2;
+          o = (n & 3) << 4 | r >> 4;
+          u = (r & 15) << 2 | i >> 6;
+          a = i & 63;
+          if (isNaN(r)) {
+              u = a = 64;
+          } else if (isNaN(i)) {
+              a = 64;
+          }
+          t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a);
+        }
+      return t;
+    },
+    decode: function(e) {
+      var t = "",
+        n, r, i,
+        s, o, u, a,
+        f = 0;
+        e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+      while (f < e.length) {
+        s = this._keyStr.indexOf(e.charAt(f++));
+        o = this._keyStr.indexOf(e.charAt(f++));
+        u = this._keyStr.indexOf(e.charAt(f++));
+        a = this._keyStr.indexOf(e.charAt(f++));
+        n = s << 2 | o >> 4;
+        r = (o & 15) << 4 | u >> 2;
+        i = (u & 3) << 6 | a;
+        t = t + String.fromCharCode(n);
+        if (u !== 64) {
+          t = t + String.fromCharCode(r);
+        }
+        if (a !==64) {
+          t = t + String.fromCharCode(i);
+        }
+      }
+      t = Base64._utf8_decode(t);
+      return t;
+    },
+    _utf8_encode: function(e) {
+      e = e.replace(/rn/g, "n");
+      var t = "";
+      for (var n = 0; n < e.length; n++) {
+        var r = e.charCodeAt(n);
+        if (r < 128) {
+          t += String.fromCharCode(r);
+        } else if (r > 127 && r < 2048) {
+          t += String.fromCharCode(r >> 6 | 192);
+          t += String.fromCharCode(r & 63 | 128);
+        } else {
+          t += String.fromCharCode(r >> 12 | 224);
+          t += String.fromCharCode(r >> 6 & 63 | 128);
+          t += String.fromCharCode(r & 63 | 128);
+        }
+      }
+      return t;
+    },
+    _utf8_decode: function(e) {
+      var t = "",
+        n = 0,
+        r = 0,
+        c3= 0,
+        c2 = 0;
+
+      while (n < e.length) {
+        r = e.charCodeAt(n);
+        if (r < 128) {
+          t += String.fromCharCode(r);
+          n++;
+        } else if (r > 191 && r < 224) {
+          c2 = e.charCodeAt(n + 1);
+          t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+          n += 2;
+        } else {
+          c2 = e.charCodeAt(n + 1);
+          c3 = e.charCodeAt(n + 2);
+          t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+          n += 3;
+        }
+      }
+      return t;
+    }
+  },
+  base64Pattern = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/;
 
   function isEncoded(str) {
     return base64Pattern.test(str);
-  };
+  }
 
   function isJson(str) {
     try {
@@ -31,11 +123,11 @@ clientStorage = (function() {
 
   function encode(val) {
     return Base64.encode(val);
-  };
+  }
 
   function decode(val) {
     return Base64.decode(val);
-  };
+  }
 
   function innerGetItem(storage, keyName) {
     var item = storage.getItem(keyName) || "";
@@ -99,10 +191,10 @@ clientStorage = (function() {
 
     for(var i = 0; i < ca.length; i++) {
       var c = ca[i];
-      while (c.charAt(0) == ' ') {
+      while (c.charAt(0) === ' ') {
         c = c.substring(1);
       }
-      if (c.indexOf(name) == 0) {
+      if (c.indexOf(name) === 0) {
         var item = decodeURIComponent(c.substring(name.length, c.length));
         if(isJson(item)) {
           item = JSON.parse(item);
@@ -138,7 +230,7 @@ clientStorage = (function() {
         return innerGetItem(sessionStorage, _keyName);
       },
       "setItem": function(_key, _value, _options) {
-        return innerSetItem(sessionStorage, _key, _value, _options)
+        return innerSetItem(sessionStorage, _key, _value, _options);
       },
       "removeItem": function(_key) {
         return innerRemoveItem(sessionStorage, _key);
@@ -156,7 +248,7 @@ clientStorage = (function() {
         return innerGetItem(localStorage, _keyName);
       },
       "setItem": function(_key, _value, _options) {
-        return innerSetItem(localStorage, _key, _value, _options)
+        return innerSetItem(localStorage, _key, _value, _options);
       },
       "removeItem": function(_key) {
         return innerRemoveItem(localStorage, _key);
@@ -183,6 +275,6 @@ clientStorage = (function() {
         return deleteAllCookies();
       }
     }
-  }
+  };
 })();
 
