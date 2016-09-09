94952b4e9e15fc57e1b9619aa576c3f53eec352d
Antonio D'Angelo <antonio.dangelo@hcl.com>
2016-09-09 17:02:50 +0200

    CXDEV-2642_AOVQuote | Summary: service PHP

--- aegon-scripts-library/aegon-angular/components/aa-qq-summary/aa-qq-summary.component.ts
+++ aegon-scripts-library/aegon-angular/components/aa-qq-summary/aa-qq-summary.component.ts
@@ -85,46 +85,36 @@ export class AAQQSummaryComponent implements OnInit {
       return false;
     }
 
-    if(location.host.search(".aegon.nl")<0) {
-      // Mock: remove when in test
-      this.emailButtonPending = false;
-      this.reSendEmailShown = true;
-      return false;  
-    }
+    // if(location.host.search(".aegon.nl")<0) {
+    //   // Mock: remove when in test
+    //   this.emailButtonPending = false;
+    //   this.reSendEmailShown = true;
+    //   return false;  
+    // }
 
-    var parser = new DOMParser(),
-        oSerializer = new XMLSerializer(),
-        XMLBodyString = '<soapenv:Envelope xmlns:bs="http://BS_Utilities_Communication/so/BS_Utilities_Communication" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Header><wsse:Security soapenv:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"><wsse:UsernameToken wsu:Id="UsernameToken-1" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><wsse:Username>AppAegonNLDrupalTST</wsse:Username><wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">dUACcFMYvwhnrnnfdq9h</wsse:Password><wsse:Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">Ztsw326A7LCpn7fOtuzlQA==</wsse:Nonce><wsu:Created>2012-04-13T12:47:09.585Z</wsu:Created></wsse:UsernameToken></wsse:Security></soapenv:Header><soapenv:Body><bs:createSendCommunication><createSendCommunicationRequest><AILHEADER><CLIENTID>MdA01</CLIENTID><CORRELATIONID>##MigrationProject##</CORRELATIONID></AILHEADER><BATCH><BATCHID>16573</BATCHID><BATCHCONFIGID>1796011752</BATCHCONFIGID><COMMUNICATIE><HEADER><ZENDING><ZENDINGVOLGNUM>1</ZENDINGVOLGNUM><ZENDINGID>000001</ZENDINGID><ZENDINGBRANDING>Aegon</ZENDINGBRANDING><KANAAL><KANAALTYPE>EMAIL</KANAALTYPE><EMAIL><_AE_FROM>noreply@aegon.nl</_AE_FROM><_AE_RETURNADRES>MAbdulahpoer@aegon.nl</_AE_RETURNADRES></EMAIL></KANAAL><DOCUMENTSET><COMMUNICATIETAAL>NL</COMMUNICATIETAAL><_AE_DOCUMENT><DOCUMENTID>AOV_EMAIL</DOCUMENTID><VOLGNUM>1</VOLGNUM><DOCUMENTBRANDING>Aegon</DOCUMENTBRANDING><_AE_DOC_DATPUB>2001-01-01</_AE_DOC_DATPUB><COLOFON><RECHTSPERSOON>ADMINISTRATIEVE DIENSTVERLENING</RECHTSPERSOON><LABELUNIT>SCP</LABELUNIT></COLOFON><ARCHIVERING><ARCHIVEREN>false</ARCHIVEREN></ARCHIVERING></_AE_DOCUMENT></DOCUMENTSET><ADRESSERING><EMAIL>#</EMAIL></ADRESSERING><ONTVANGER><_AE_TYPEPARTIJ>BEDRIJF</_AE_TYPEPARTIJ><_AE_BEDRIJFSNAAM>Uw tussenpersoon</_AE_BEDRIJFSNAAM></ONTVANGER></ZENDING><HERKOMST><SYSTEEMID>HA</SYSTEEMID><GEBRUIKER></GEBRUIKER><OMGEVING>T</OMGEVING><BATCHNUMMER>123456786</BATCHNUMMER><BATCHFREQUENTIE>MAANDELIJKS</BATCHFREQUENTIE></HERKOMST></HEADER><_AE_DOC_DATA><DocumentData><Geboortedatum>#</Geboortedatum><Beroep>#</Beroep><Bruto_Jaarinkomen>#</Bruto_Jaarinkomen><Eigen_risicoperiode>#</Eigen_risicoperiode><Verzekerdbedrag>#</Verzekerdbedrag><Bruto_premie_permaand>#</Bruto_premie_permaand><Netto_premie_permaand>#</Netto_premie_permaand></DocumentData></_AE_DOC_DATA></COMMUNICATIE></BATCH></createSendCommunicationRequest></bs:createSendCommunication></soapenv:Body></soapenv:Envelope>';
+    let dataReq = {
+       "Email" : this.emailAddress,
+        "Geboortedatum" : this.aov_qq_data.birthDate,
+        "Beroep" : this.aov_qq_data.profession,
+        "Bruto_Jaarinkomen" : this.aov_qq_data.grossYearAmount,
+        "Eigen_risicoperiode" : this.aov_qq_data.startingTerm,
+        "Verzekerdbedrag" : this.aov_qq_data.insuranceAmount,
+        "Bruto_premie_permaand" : this.aov_qq_data.grossMonthly,
+        "Netto_premie_permaand" : this.aov_qq_data.netMonthly
+    };
 
-    let XMLBody = parser.parseFromString(XMLBodyString,"text/xml");
-    let headers = new Headers({
-      'Content-Type': 'application/xml',
-      'charset': 'utf-8',
-      'SOAPAction': this.mailUrl
-    });
-
-    //Set SOAP Body with values
-    XMLBody.getElementsByTagName("CORRELATIONID")[0].childNodes[0].nodeValue = "##" + Date.now() + "##";
-    XMLBody.getElementsByTagName("ADRESSERING")[0].getElementsByTagName("EMAIL")[0].childNodes[0].nodeValue = this.emailAddress;
-    XMLBody.getElementsByTagName("Geboortedatum")[0].childNodes[0].nodeValue = this.aov_qq_data.birthDate;
-    XMLBody.getElementsByTagName("Beroep")[0].childNodes[0].nodeValue = this.aov_qq_data.profession;
-    XMLBody.getElementsByTagName("Bruto_Jaarinkomen")[0].childNodes[0].nodeValue = this.aov_qq_data.grossYearAmount;
-    XMLBody.getElementsByTagName("Eigen_risicoperiode")[0].childNodes[0].nodeValue = this.aov_qq_data.startingTerm;
-    XMLBody.getElementsByTagName("Verzekerdbedrag")[0].childNodes[0].nodeValue = this.aov_qq_data.insuranceAmount;
-    XMLBody.getElementsByTagName("Bruto_premie_permaand")[0].childNodes[0].nodeValue = this.aov_qq_data.grossMonthly;
-    XMLBody.getElementsByTagName("Netto_premie_permaand")[0].childNodes[0].nodeValue = this.aov_qq_data.netMonthly;
-
+    let headers = new Headers({'Content-Type': 'application/json'});
     let options = new RequestOptions({headers: headers});
+    this.http.post(location.host+this.mailUrl, JSON.stringify(dataReq), options)
+      .map(res => res.json())
+      .catch(this.handleError)
+      .subscribe(data => {
+        this.emailButtonPending = false;
+        this.reSendEmailShown = true;
+        console.log(data);
+      }, error => console.error(error));
 
-    this.http.post(this.mailUrl , oSerializer.serializeToString(XMLBody), options)
-        .map(res => res.json())
-        .catch(this.handleError)
-        .subscribe(data => {
-          this.emailButtonPending = false;
-          this.reSendEmailShown = true;
-        }, error => console.error(error));
-
-    return false;
+      return false;
   }
 
 }
