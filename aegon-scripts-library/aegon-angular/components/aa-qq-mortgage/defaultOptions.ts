519d478743d27b343f8883384aac6c7ede772635
Ben Besuijen <ben.besuijen@aegon.nl>
2016-10-07 17:10:39 +0200

    Made copy of older Mortgage component. Refactored with new conventions. Work in progress

---
+++ aegon-scripts-library/aegon-angular/components/aa-qq-mortgage/defaultOptions.ts
@@ -0,0 +1,10 @@
+export const defaultOptions = {
+  title: 'Summary',
+  mailUrl: '/data/email/sendEmail',
+  mailCredentials: 'AppAegonNLDrupalTST:dUACcFMYvwhnrnnfdq9h',
+  actionButton: {
+  	url: 'https://www.aegon.nl/zakelijk/inkomensverzekeringen/arbeidsongeschiktheidsverzekering/afspraak-arbeidsongeschiktheidsverzekering-advies',
+  	label: 'Neem contact met mij op'
+  },
+  goBackAndChangeUrl: '/aov-premie-berekenen-flexible'
+};
