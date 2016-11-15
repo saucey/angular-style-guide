e7a5b36dc9f0a00eaa99c0cd6612903cf24ca9f4
Mageshwaran <mageshwaran.viswanathan@aegon.com>
2016-11-15 11:58:08 +0100

    Squashed commit of the following:
    commit ee8318912d2024b2233467b159d8f9c50ca1a5e3
    Merge: f14e2c0 82f12a7
    Author: Mageshwaran <mageshwaran.viswanathan@aegon.com>
    Date:   Tue Nov 15 10:18:58 2016 +0100
    
        Merge branch 'feature/CXDEVIII-1852' of ssh://swfactory.aegon.com:7999/web/aegon-frontend-library into feature/CXDEVIII-1852
    
    commit f14e2c0c01ea92eed52d5f0d585498f922c8b365
    Merge: 52ad163 eb6333f
    Author: Mageshwaran <mageshwaran.viswanathan@aegon.com>
    Date:   Tue Nov 15 10:15:53 2016 +0100
    
        Merge branch 'master' of ssh://swfactory.aegon.com:7999/web/aegon-frontend-library into feature/CXDEVIII-1852
    
    commit 52ad163737af623d54fbe7dffb7cabbe129ade49
    Author: Mageshwaran <mageshwaran.viswanathan@aegon.com>
    Date:   Tue Nov 15 10:13:37 2016 +0100
    
        CXDEVIII-1852 basic layout setup
    
    commit 82f12a72b410c0359e491d1684ba848bc0c15b42
    Author: Maciej Dudzinski <maciej.dudzinksi@aegon.nl>
    Date:   Fri Nov 11 16:13:15 2016 +0100
    
        CXDEVIII-1852: Add mock response
    
    commit 0e1cd86110353a1a0a263d823fdc20e16048f111
    Author: Maciej Dudzinski <maciej.dudzinksi@aegon.nl>
    Date:   Fri Nov 11 16:13:15 2016 +0100
    
        CXDEVIII-1852: Add mock response

---
+++ aegon-scripts-library/aegon-angular/modules/fnol-repairshop/fnol-repairshop.component.ts
@@ -0,0 +1,15 @@
+import {
+  Component
+} from '@angular/core';
+
+const template = require('./template.html');
+
+@Component({
+  selector: 'fnol-repairshop',
+  template: template,
+  providers: []
+})
+
+export class FNOLRepairshopComponent {
+}
+
