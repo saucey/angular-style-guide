0d474e9d85b1b46dea2124eecf46ad033d7fbe5c
Antonio D'Angelo <antonio.dangelo@hcl.com>
2016-11-15 14:40:13 +0100

    CXDEV-3010 | request-an-offer-for-chosen-dip: Text editable

--- aegon-scripts-library/aegon-angular/components/aa-pension-offer-form/defaultOptions.ts
+++ aegon-scripts-library/aegon-angular/components/aa-pension-offer-form/defaultOptions.ts
@@ -2,5 +2,15 @@ export const defaultOptions = {
   start: {
     url: '/page-to-redirect-when-no-session-storage',
     redirect: false
-  }  
+  },
+  subtitle: "Vul hieronder uw gegevens in. U ontvangt dan van ons een offerte per mail.",
+  termsTitle: "Voorwaarden offerte aanvraag",
+  termsList: [
+    "U heeft als werknemer een pensioenkapitaal opgebouwd.",
+    "Het vrijgekomen bedrag is €25.000 of hoger.",
+    "De pensioenuitkering moet uiterlijk over 3 maanden ingaan."
+  ],
+  yourDataTitle: "Uw gegevens",
+  yourDataText: "Na het versturen van uw aanvraag ontvangt de offerte binnen 5 dagen per brief bij u thuis. Uw adviseur neemt later contact met u op en kijkt samen met u wat het beste bij u past. Daarna kunt u een afspraak maken voor een persoonlijk gesprek.",
+  
 }
