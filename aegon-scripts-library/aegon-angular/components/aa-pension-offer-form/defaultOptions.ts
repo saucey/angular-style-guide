0aaa0720702911a450553211656e05e55163b477
Ben Besuijen <ben.besuijen@hcl.com>
2016-11-16 14:41:33 +0100

    feature/CXDEV-3010-request-an-offer-for-chosen-dip | Put sticky options in own object

--- aegon-scripts-library/aegon-angular/components/aa-pension-offer-form/defaultOptions.ts
+++ aegon-scripts-library/aegon-angular/components/aa-pension-offer-form/defaultOptions.ts
@@ -2,21 +2,7 @@ export const defaultOptions = {
   start: {
     url: '/page-to-redirect-when-no-session-storage',
     redirect: false
-  },
-  title: 'Samenvatting',
-  helpers: {
-    grossPerMonth: 'Bedragen bruto per maand'
-  },
-  labels: {
-    pension: ['Pensioenuitkering', 'Partnerpensioenuitkering'],
-    pensionProduct: 'Uitkerend pensioenproduct',
-    pensionAmount: 'Opgebouwd pensioenkapitaal',
-    pensionLocation: 'Pensioen opgebouwd',
-    pensionPartner: 'Pensioen partner',
-    startingDate: 'Startdatum uitkering',
-    birthDate: 'Geboortedatum',
-    birthDateOfPartner: 'Geboortedatum partner'
-  },
+  },  
   subtitle: "Vul hieronder uw gegevens in. U ontvangt dan van ons een offerte per mail.",
   termsTitle: "Voorwaarden offerte aanvraag",
   termsList: [
@@ -29,8 +15,24 @@ export const defaultOptions = {
   values: {
     pensionLocation: ['Bij Aegon', 'Niet bij Aegon', 'Aegon en elders']
   },
-  link: {
-    text: 'Uw gegevens aanpassen',
-    url: '/link-to-edit'
+  sticky: {
+    title: 'Samenvatting',
+    helpers: {
+      grossPerMonth: 'Bedragen bruto per maand'
+    },
+    labels: {
+      pension: ['Pensioenuitkering', 'Partnerpensioenuitkering'],
+      pensionProduct: 'Uitkerend pensioenproduct',
+      pensionAmount: 'Opgebouwd pensioenkapitaal',
+      pensionLocation: 'Pensioen opgebouwd',
+      pensionPartner: 'Pensioen partner',
+      startingDate: 'Startdatum uitkering',
+      birthDate: 'Geboortedatum',
+      birthDateOfPartner: 'Geboortedatum partner'
+    },
+    link: {
+      text: 'Uw gegevens aanpassen',
+      url: '/link-to-edit'
+    }
   }
 }
