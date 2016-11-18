dfbae8c885cb53a9e36c5e60e2ba02e99aacea44
Antonio D'Angelo <antonio.dangelo@hcl.com>
2016-11-18 13:32:25 +0100

    CXDEV-3010 | request-an-offer-for-chosen-dip: Label telephon number

--- aegon-scripts-library/aegon-angular/components/aa-pension-offer-form/defaultOptions.ts
+++ aegon-scripts-library/aegon-angular/components/aa-pension-offer-form/defaultOptions.ts
@@ -29,6 +29,9 @@ export const defaultOptions = {
     link: {
       text: 'Uw gegevens aanpassen',
       url: '/link-to-edit'
+    },
+    formLabels : {
+      telephon: 'Telefoon nummer'
     }
   }
 }
