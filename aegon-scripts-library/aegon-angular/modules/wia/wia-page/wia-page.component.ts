1d786ec133a46d9c2649f0499b96edc992b9a257
Florian Popa <florian.popa@aegon.nl>
2016-12-02 14:38:32 +0100

    CXDEVIII-2063: Implement monitoring for WIA.

--- aegon-scripts-library/aegon-angular/modules/wia/wia-page/wia-page.component.ts
+++ aegon-scripts-library/aegon-angular/modules/wia/wia-page/wia-page.component.ts
@@ -1,15 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, AfterViewInit } from "@angular/core";
 import { CalculatorDataService } from "../wia-calculator/wia-calculator-data.service";
+import { WIATealiumService } from "./wia-tealium.service";
 
 @Component({
   selector: 'aa-wia-page',
-  providers: [CalculatorDataService],
+  providers: [
+    CalculatorDataService,
+    WIATealiumService
+  ],
   template: '<ng-content></ng-content>'
 })
 
 /**
  * The parent component for the Wia page.
  */
-export class WiaPageComponent {
+export class WiaPageComponent implements AfterViewInit {
 
+  constructor (private wiaTealiumService: WIATealiumService) {}
+
+  ngAfterViewInit(): void {
+    this
+      .wiaTealiumService
+      .wiaToolLoaded();
+  }
 }
