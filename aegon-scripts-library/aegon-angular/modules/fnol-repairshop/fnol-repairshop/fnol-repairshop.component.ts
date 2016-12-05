32011d58d43e1aef3f7c4fbcbac83319e7aa110f
Mageshwaran <mageshwaran.viswanathan@aegon.com>
2016-12-05 15:49:40 +0100

    CXDEVIII-2182: Validation for FNOL form.

--- aegon-scripts-library/aegon-angular/modules/fnol-repairshop/fnol-repairshop/fnol-repairshop.component.ts
+++ aegon-scripts-library/aegon-angular/modules/fnol-repairshop/fnol-repairshop/fnol-repairshop.component.ts
@@ -1,5 +1,5 @@
 import {
-    Component, Input
+  Component, Input
 } from '@angular/core';
 
 import {FnolRepairshopService} from "../shared/services/fnol.data.service";
@@ -10,80 +10,76 @@ const INFO_FORM: string = 'info_form';
 const RESULT_MOBILE: string = 'result_mobile';
 
 @Component({
-    selector: 'fnol-repairshop',
-    template: template,
-    providers: [FnolRepairshopService]
+  selector: 'fnol-repairshop',
+  template: template,
+  providers: [FnolRepairshopService]
 })
 
 export class FNOLRepairshopComponent {
 
-    public isHideRepairshopResults: boolean = true;
-    public isHideMobileRepairshopResults: boolean = true;
-    public isShowPaginateInfo: boolean = false;
-    public getItemShown: string = INFO_FORM;
-    public parties = [];
-    public sortedBy = 'ranking';
-  // address.radius
-  // name
-    public sortDirection = 'DESC';
-    // DESC
+  public isHideRepairshopResults: boolean = true;
+  public isHideMobileRepairshopResults: boolean = true;
+  public isShowPaginateInfo: boolean = false;
+  public getItemShown: string = INFO_FORM;
+  public parties = [];
+  public sortedBy = 'ranking';
+  public sortDirection = 'DESC';
 
-    public repairshop = {
-        postcode: null,
-        distance: null,
-        damage: null
-    };
 
-    constructor(private fnolRepairshopService: FnolRepairshopService) {}
+  public repairshop = {
+    postcode: null,
+    distance: 10,
+    damage: null
+  };
 
-    public repairshopFormSubmit() {
-        this
-            .fnolRepairshopService
-            .getData({
-                location: this.repairshop.postcode,
-                radius: this.repairshop.distance,
-                type: this.repairshop.damage
-            })
-            .subscribe(results => {
-                this.parties = results;
-                this.getRepairshopSearchData();
-                this.showPaginateInfo();
-                this.getItemShown = RESULT_MOBILE;
-            });
+  constructor(private fnolRepairshopService: FnolRepairshopService) {
+  }
+
+  public repairshopFormSubmit() {
+    this
+      .fnolRepairshopService
+      .getData({
+        location: this.repairshop.postcode,
+        radius: this.repairshop.distance,
+        type: this.repairshop.damage
+      })
+      .subscribe(results => {
+        this.parties = results;
+        this.getRepairshopSearchData();
+        this.showPaginateInfo();
+        this.getItemShown = RESULT_MOBILE;
+      });
+  }
+
+  getRepairshopSearchData() {
+    this.isHideRepairshopResults = false;
+    this.isHideMobileRepairshopResults = false;
+  }
+
+  hideMobileResults() {
+    this.isHideMobileRepairshopResults = true;
+    this.getItemShown = INFO_FORM;
+  }
+
+  showPaginateInfo() {
+    if (this.parties.length >= 10) {
+      this.isShowPaginateInfo = true;
+    } else {
+      this.isShowPaginateInfo = false;
     }
+  }
 
-    getRepairshopSearchData() {
-       this.isHideRepairshopResults = false;
-       this.isHideMobileRepairshopResults = false;
-    }
-
-    hideMobileResults() {
-        this.isHideMobileRepairshopResults = true;
-        this.getItemShown = INFO_FORM;
-    }
-
-    showPaginateInfo() {
-      if (this.parties.length >= 10){
-        this.isShowPaginateInfo = true;
-      }else{
-        this.isShowPaginateInfo = false;
-      }
-    }
-
-    public sortByKey(sortProperty) {
-      if (this.sortedBy === sortProperty) {
-        if (this.sortDirection === 'ASC') {
-          this.sortDirection = 'DESC';
-        } else {
-          this.sortDirection = 'ASC';
-        }
+  public sortByKey(sortProperty) {
+    if (this.sortedBy === sortProperty) {
+      if (this.sortDirection === 'ASC') {
+        this.sortDirection = 'DESC';
       } else {
-        this.sortedBy = sortProperty;
         this.sortDirection = 'ASC';
       }
+    } else {
+      this.sortedBy = sortProperty;
+      this.sortDirection = 'ASC';
     }
-
-
-
+  }
 }
 
