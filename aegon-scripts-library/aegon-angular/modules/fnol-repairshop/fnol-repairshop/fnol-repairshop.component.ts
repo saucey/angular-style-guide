52acd0430c6d3d75d926fa9358d4004bb1002894
DADM Kathiresan <dadmldrijj1@US.AEGON.COM>
2016-12-07 11:01:55 +0100

    CXDEVIII-2182 - Fnol UAT

--- aegon-scripts-library/aegon-angular/modules/fnol-repairshop/fnol-repairshop/fnol-repairshop.component.ts
+++ aegon-scripts-library/aegon-angular/modules/fnol-repairshop/fnol-repairshop/fnol-repairshop.component.ts
@@ -85,5 +85,39 @@ export class FNOLRepairshopComponent {
       this.sortDirection = 'ASC';
     }
   }
+
+  fnolPagination(){
+
+
+  }
+
+}
+
+export class PagerService {
+  public parties = [];
+  getPager(totalItems: number = this.parties.length, currentPage: number = 1, pageSize: number = 10) {
+    // calculate total pages
+    let totalPages = Math.ceil(totalItems / pageSize);
+
+    let startPage: number, endPage: number;
+    if (totalPages <= 10) {
+      // less than 10 total pages so show all
+      startPage = 1;
+      endPage = totalPages;
+    }
+    else {
+      // more than 10 total pages so calculate start and end pages
+      if (currentPage <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (currentPage + 4 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 5;
+        endPage = currentPage + 4;
+      }
+    }
+}
 }
 
